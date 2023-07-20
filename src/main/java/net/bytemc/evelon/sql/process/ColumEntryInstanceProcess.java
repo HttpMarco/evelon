790dd004a1e0c054fb6c3f8e4e56cd2bf8d4ee21
HttxMarco <mircolindenau04@gmail.com>
2023-07-20 15:27:59 +0200

    Implement: first setup of select statement

--- src/main/java/net/bytemc/evelon/sql/process/ColumEntryInstanceProcess.java
+++ src/main/java/net/bytemc/evelon/sql/process/ColumEntryInstanceProcess.java
@@ -3,10 +3,7 @@ package net.bytemc.evelon.sql.process;
 import net.bytemc.evelon.misc.Pair;
 import net.bytemc.evelon.repository.Repository;
 import net.bytemc.evelon.repository.RepositoryClass;
-import net.bytemc.evelon.sql.DatabaseHelper;
-import net.bytemc.evelon.sql.Stage;
-import net.bytemc.evelon.sql.StageHandler;
-import net.bytemc.evelon.sql.SubElementStage;
+import net.bytemc.evelon.sql.*;
 
 import java.lang.reflect.Field;
 import java.util.ArrayList;
@@ -16,9 +13,22 @@ public final class ColumEntryInstanceProcess {
 
     public static void collect(Repository<?> repository) {
 
-        //  select select_list
-        // from t1
-        //  inner join t2 using (column1);
+        List<String> neededTables = getNeededTables(repository.getName(), repository.repositoryClass());
+
+        StringBuilder query = new StringBuilder("SELECT * FROM " + neededTables.remove(0) + " %S;");
+
+
+        var primaryNames = String.join(", ", repository.repositoryClass().getPrimaries().stream().map(DatabaseHelper::getRowName).toList());
+        var innerJoins = String.join(" ", neededTables.stream().map(it -> "INNER JOIN polus_element USING (" + primaryNames + ")").toList());
+
+        DatabaseConnection.executeQuery(query.toString().formatted(innerJoins), resultSet -> {
+
+            while (resultSet.next()) {
+                System.out.println("polo");
+            }
+            return null;
+        }, null);
+
     }
 
     private static List<String> getNeededTables(String children, RepositoryClass<?> clazz) {
