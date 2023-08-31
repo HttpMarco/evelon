9cfc68dba1b65bf8b73da19a266479d57379b130
Thies <thies.hoelting@gmail.com>
2023-08-31 13:22:11 +0200

    added sync all with filters startup property

--- src/main/java/net/bytemc/evelon/repository/properties/StartupProperties.java
+++ src/main/java/net/bytemc/evelon/repository/properties/StartupProperties.java
@@ -1,5 +1,6 @@
 package net.bytemc.evelon.repository.properties;
 
+import net.bytemc.evelon.repository.Filter;
 import net.bytemc.evelon.repository.Repository;
 
 public interface StartupProperties<T> extends Properties {
@@ -10,6 +11,10 @@ public interface StartupProperties<T> extends Properties {
 
     static <T> StartupProperties<T> syncAll() {
         return new SyncAllStartupProperty<>();
+    }
+
+    static <T> StartupProperties<T> syncAllWithFilters(Filter... filters) {
+        return new SyncAllWithFiltersStartupProperty<>(filters);
     }
 
     void initialize(Repository<T> repository);
