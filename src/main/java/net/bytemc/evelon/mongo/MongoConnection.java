7ba34a82770389492fc63a693911fc0f1d78e3ab
HabsGleich <nick@defever.de>
2023-09-08 00:20:49 +0200

    Implement jackson to convert objects into bson

--- src/main/java/net/bytemc/evelon/mongo/MongoConnection.java
+++ src/main/java/net/bytemc/evelon/mongo/MongoConnection.java
@@ -1,5 +1,6 @@
 package net.bytemc.evelon.mongo;
 
+import com.mongodb.MongoClientSettings;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
@@ -9,8 +10,12 @@ import lombok.NoArgsConstructor;
 import net.bytemc.evelon.Debugger;
 import net.bytemc.evelon.Evelon;
 import net.bytemc.evelon.cradinates.DatabaseCradinates;
+import net.bytemc.evelon.mongo.codec.EvelonCodecProvider;
+import net.bytemc.evelon.mongo.codec.ObjectMapperFactory;
 import net.bytemc.evelon.repository.Repository;
 import org.bson.Document;
+import org.bson.codecs.configuration.CodecRegistries;
+import org.bson.codecs.configuration.CodecRegistry;
 import org.jetbrains.annotations.ApiStatus;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
@@ -31,8 +36,13 @@ public final class MongoConnection {
             cradinates.port(),
             cradinates.database()
         )).getDatabase(cradinates.database());*/
+
         // TODO: Only for local testing reasons
-        database = MongoClients.create().getDatabase(cradinates.database());
+        final CodecRegistry codecRegistry = CodecRegistries.fromProviders(
+            CodecRegistries.fromRegistries(MongoClientSettings.getDefaultCodecRegistry()),
+            new EvelonCodecProvider(ObjectMapperFactory.create())
+        );
+        database = MongoClients.create().getDatabase(cradinates.database()).withCodecRegistry(codecRegistry);
         Debugger.log("Established connection to mongodb server");
     }
 
