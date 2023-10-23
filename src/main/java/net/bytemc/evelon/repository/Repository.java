4115dc02d4ac7c5c088cabfdcc55eeb44f7e6aa7
HttpMarco <mirco.lindenau@gmx.de>
2023-10-23 21:59:57 +0200

    Add mysql for table selection

--- src/main/java/net/bytemc/evelon/repository/Repository.java
+++ src/main/java/net/bytemc/evelon/repository/Repository.java
@@ -47,7 +47,7 @@ public record Repository<T>(RepositoryClass<T> repositoryClass, StartupPropertie
         StorageHandler.getStorage(LocalStorage.class).initializeRepository(repository);
 
         switch (Evelon.getDatabaseCradinates().databaseProtocol()) {
-            case MARIADB, H2 -> {
+            case MARIADB, H2, MYSQL -> {
                 // check if table exists in sql, else create table
                 // check also old table names, because the table can be renamed and not lose the data.
                 if (!SQLHelper.isTableExists(repository.getName()) && !SQLHelper.isTableExists(repository.repositoryClass.clazz().getSimpleName().toLowerCase())) {
