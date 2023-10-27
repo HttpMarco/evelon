c4d9da6f6b73a168368c601af6b29b4cd0621997
HttpMarco <mirco.lindenau@gmx.de>
2023-10-27 23:08:51 +0200

    Add locale date time transformer

---
+++ src/main/java/net/bytemc/evelon/sql/transformers/LocaleDateTransformerSQL.java
@@ -0,0 +1,33 @@
+package net.bytemc.evelon.sql.transformers;
+
+import net.bytemc.evelon.sql.SQLElementStageTransformer;
+import net.bytemc.evelon.sql.Stage;
+import net.bytemc.evelon.sql.StageHandler;
+import net.bytemc.evelon.sql.stages.DateStageSQL;
+
+import java.sql.Date;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public final class LocaleDateTransformerSQL implements SQLElementStageTransformer<LocalDate> {
+
+    @Override
+    public Stage<?> transformTo() {
+        return StageHandler.getInstance().getStage(DateStageSQL.class);
+    }
+
+    @Override
+    public Object transform(LocalDate value) {
+        return Date.valueOf(value);
+    }
+
+    @Override
+    public LocalDate rollback(Object value) {
+        return ((Date) value).toLocalDate();
+    }
+
+    @Override
+    public boolean isElement(Class<?> type) {
+        return type.equals(LocalDate.class);
+    }
+}
