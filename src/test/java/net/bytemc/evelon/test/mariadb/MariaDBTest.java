09fb845113aaa497b5531bd68f8ff10fb8ec5cd7
HttxMarco <mircolindenau04@gmail.com>
2023-10-31 11:34:25 +0100

    Disable tests

--- src/test/java/net/bytemc/evelon/test/mariadb/MariaDBTest.java
+++ src/test/java/net/bytemc/evelon/test/mariadb/MariaDBTest.java
@@ -4,13 +4,16 @@ import net.bytemc.evelon.DatabaseProtocol;
 import net.bytemc.evelon.Debugger;
 import net.bytemc.evelon.Evelon;
 import net.bytemc.evelon.test.DefaultTest;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledIf;
 
 import java.util.UUID;
 
 public class MariaDBTest {
 
     @Test
+    @Disabled
     public void test() {
         Debugger.setEnable(true);
         Evelon.setCradinates(DatabaseProtocol.MARIADB, "127.0.0.1", "", "root", "tnw", 3306);
