d0411da5210e691c8b135c1e9b7037db2d555049
HttxMarco <mircolindenau04@gmail.com>
2023-10-31 10:21:32 +0100

    Update final tests

--- src/test/java/net/bytemc/evelon/test/TestRepository.java
+++ src/test/java/net/bytemc/evelon/test/TestRepository.java
@@ -1,8 +1,10 @@
 package net.bytemc.evelon.test;
 
 import lombok.*;
+import net.bytemc.evelon.repository.Repository;
 import net.bytemc.evelon.repository.annotations.Entity;
 import net.bytemc.evelon.repository.annotations.PrimaryKey;
+import net.bytemc.evelon.repository.properties.StartupProperties;
 
 import java.util.UUID;
 
@@ -11,6 +13,9 @@ import java.util.UUID;
 @AllArgsConstructor
 @Entity(name = "polus2")
 public class TestRepository {
+
+    public static final Repository<TestRepository> REPOSITORY = Repository.create(TestRepository.class, StartupProperties.syncAll());
+    public static final TestRepository TEST_REPO = new TestRepository("HabsGleich", UUID.randomUUID(), 64);
 
     @PrimaryKey
     private String name;
