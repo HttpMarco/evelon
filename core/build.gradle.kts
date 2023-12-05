b79ab3f1b6b37370d3bb004d802d5e02e3fda70f
HttpMarco <mirco.lindenau@gmx.de>
2023-12-05 22:48:56 +0100

    Fix: annotation lib is missing

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -10,6 +10,7 @@ repositories {
 }
 
 dependencies {
+    compileOnly("org.jetbrains:annotations:24.0.1")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
 }
