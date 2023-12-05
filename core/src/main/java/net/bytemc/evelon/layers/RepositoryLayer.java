d3f7547424d85dc4b10d0a854356bfa86cc815f9
HttxMarco <mircolindenau04@gmail.com>
2023-12-05 14:30:59 +0100

    Implement defualt layer method and add this to local storage

--- core/src/main/java/net/bytemc/evelon/layers/RepositoryLayer.java
+++ core/src/main/java/net/bytemc/evelon/layers/RepositoryLayer.java
@@ -1,4 +1,16 @@
 package net.bytemc.evelon.layers;
 
+import net.bytemc.evelon.query.DataQuery;
+
 public interface RepositoryLayer {
+
+    /**
+     * Creates a new data entry based on the specified query and value.
+     *
+     * @param query The DataQuery object representing the query for creating the data entry.
+     * @param value The value of type T that will be associated with the data entry.
+     * @param <T>   The type parameter indicating the class type of the value.
+     */
+    <T> void create(DataQuery<T> query, T value);
+
 }
--- core/src/main/java/net/bytemc/evelon/local/LocalStorageLayer.java
+++ core/src/main/java/net/bytemc/evelon/local/LocalStorageLayer.java
@@ -1,7 +1,23 @@
 package net.bytemc.evelon.local;
 
-public class LocalStorageLayer {
+import net.bytemc.evelon.layers.RepositoryLayer;
+import net.bytemc.evelon.query.DataQuery;
+import net.bytemc.evelon.repository.Repository;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+public final class LocalStorageLayer implements RepositoryLayer {
 
+    private static final Map<Repository<?>, List<?>> LOCAL_POOL = new HashMap<>();
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public <T> void create(DataQuery<T> query, T value) {
+        var currentData = (List<T>) LOCAL_POOL.getOrDefault(query.getRepository(), new ArrayList<>());
+        currentData.add(value);
+        LOCAL_POOL.put(query.getRepository(), currentData);
+    }
 }
