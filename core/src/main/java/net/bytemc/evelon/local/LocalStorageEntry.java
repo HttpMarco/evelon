7d5c0c74e38774185415d740a02a769837ea1cc5
HttpMarco <mirco.lindenau@gmx.de>
2023-12-05 19:27:36 +0100

    Add expire function for data entries

--- core/src/main/java/net/bytemc/evelon/local/LocalStorageEntry.java
+++ core/src/main/java/net/bytemc/evelon/local/LocalStorageEntry.java
@@ -3,7 +3,13 @@ package net.bytemc.evelon.local;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.concurrent.TimeUnit;
+
 public record LocalStorageEntry<T>(long creationTime, T value) {
+
+    public boolean isExpired(TimeUnit type, long time) {
+        return creationTime + type.toMillis(time) >= System.currentTimeMillis();
+    }
 
     @Contract("_ -> new")
     public static <T> @NotNull LocalStorageEntry<T> of(T value) {
