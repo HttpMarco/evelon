a1117cad127b33e2c87c88e5d2685cf1b3f5e5c1
HttpMarco <mirco.lindenau@gmx.de>
2023-12-06 23:14:02 +0100

    Modify local filter

--- core/src/main/java/net/bytemc/evelon/local/LocalFilterHandler.java
+++ core/src/main/java/net/bytemc/evelon/local/LocalFilterHandler.java
@@ -3,90 +3,53 @@ package net.bytemc.evelon.local;
 import net.bytemc.evelon.filters.Filter;
 import net.bytemc.evelon.filters.LayerFilterHandler;
 
-public class LocalFilterHandler implements LayerFilterHandler {
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public final class LocalFilterHandler implements LayerFilterHandler<Boolean, Object> {
 
     @Override
-    public Filter min(String id, int min) {
+    public Filter<Boolean, Object> min(String id, int min) {
         return null;
     }
 
     @Override
-    public Filter max(String id, int max) {
+    public Filter<Boolean, Object> max(String id, int max) {
         return null;
     }
 
     @Override
-    public Filter between(String id, int min, int max) {
+    public Filter<Boolean, Object> between(String id, int min, int max) {
         return null;
     }
 
     @Override
-    public Filter match(String id, Object value) {
+    public Filter<Boolean, Object> match(String id, Object value) {
         return null;
     }
 
     @Override
-    public Filter nonMatch(String id, Object values) {
+    public Filter<Boolean, Object> nonMatch(String id, Object values) {
         return null;
     }
 
     @Override
-    public Filter like(String id, String value) {
+    public Filter<Boolean, Object> like(String id, String value) {
         return null;
     }
 
     @Override
-    public Filter sameDay(String id, String date) {
+    public Filter<Boolean, Object> someDate(String id, Date date, TimeUnit timeUnit) {
         return null;
     }
 
     @Override
-    public Filter sameMonth(String id, String date) {
+    public Filter<Boolean, Object> betweenTime(String id, Date date, String date2) {
         return null;
     }
 
     @Override
-    public Filter sameYear(String id, String date) {
-        return null;
-    }
-
-    @Override
-    public Filter sameWeek(String id, String date) {
-        return null;
-    }
-
-    @Override
-    public Filter sameQuarter(String id, String date) {
-        return null;
-    }
-
-    @Override
-    public Filter sameHour(String id, String date) {
-        return null;
-    }
-
-    @Override
-    public Filter sameMinute(String id, String date) {
-        return null;
-    }
-
-    @Override
-    public Filter sameSecond(String id, String date) {
-        return null;
-    }
-
-    @Override
-    public Filter sameMillisecond(String id, String date) {
-        return null;
-    }
-
-    @Override
-    public Filter betweenTime(String id, String date1, String date2) {
-        return null;
-    }
-
-    @Override
-    public Filter sameTime(String id, String date) {
+    public Filter<Boolean, Object> sameTime(String id, Date date) {
         return null;
     }
 }
