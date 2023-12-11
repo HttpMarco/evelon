c8a987929cff304090dd3bdd963041adc6326308
HttpMarco <mirco.lindenau@gmx.de>
2023-12-11 18:38:21 +0100

    Gradle settings

--- sql-parent/build.gradle.kts
+++ sql-parent/build.gradle.kts
@@ -10,6 +10,7 @@ repositories {
 }
 
 dependencies {
+    compileOnly(project(":core"))
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
 }
