c4c029f32a736890cebba6cdbb27d70563707067
HttpMarco <mirco.lindenau@gmx.de>
2024-01-24 19:34:17 +0100

    Add missing generic

--- evelon-common/src/main/java/dev/httpmarco/evelon/common/repository/RepositoryImpl.java
+++ evelon-common/src/main/java/dev/httpmarco/evelon/common/repository/RepositoryImpl.java
@@ -1,5 +1,6 @@
 package dev.httpmarco.evelon.common.repository;
 
+import dev.httpmarco.evelon.common.repository.clazz.RepositoryClass;
 import dev.httpmarco.evelon.common.repository.clazz.RepositoryClassImpl;
 import lombok.Getter;
 import lombok.experimental.Accessors;
@@ -13,10 +14,10 @@ import java.util.List;
 public final class RepositoryImpl<T> implements Repository<T> {
 
     private final List<LocalStorageEntry<T>> localData = new ArrayList<>();
-    private final RepositoryClass<?> clazz;
+    private final RepositoryClass<T> clazz;
 
     public RepositoryImpl(Class<T> clazz) {
-        this.clazz = new RepositoryClassImpl(clazz);
+        this.clazz = new RepositoryClassImpl<>(clazz);
     }
 
 }
