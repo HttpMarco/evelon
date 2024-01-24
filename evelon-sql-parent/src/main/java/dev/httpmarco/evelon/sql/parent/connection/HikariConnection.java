ed1554949291f923a0c87337c519b936be2d0474
HttpMarco <mirco.lindenau@gmx.de>
2024-01-24 19:06:25 +0100

    Test hikari connection tree

---
+++ evelon-sql-parent/src/main/java/dev/httpmarco/evelon/sql/parent/connection/HikariConnection.java
@@ -0,0 +1,12 @@
+package dev.httpmarco.evelon.sql.parent.connection;
+
+import java.sql.Connection;
+
+public class HikariConnection {
+
+    public Connection getConnection() {
+        //todo
+        return null;
+    }
+
+}
