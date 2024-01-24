8f8a67efc59d3d049e399c94d0f191827aee4825
HttxMarco <mircolindenau04@gmail.com>
2024-01-24 12:24:47 +0100

    Add transmitter information

--- evelon-sql-parent/src/main/java/dev/httpmarco/evelon/sql/parent/connection/StatementTransmitter.java
+++ evelon-sql-parent/src/main/java/dev/httpmarco/evelon/sql/parent/connection/StatementTransmitter.java
@@ -3,6 +3,7 @@ package dev.httpmarco.evelon.sql.parent.connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
+@FunctionalInterface
 public interface StatementTransmitter {
 
     void result(PreparedStatement statement) throws SQLException;
