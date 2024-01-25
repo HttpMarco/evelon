fd23e0f733c0a9c30b1099354b0945ec71e5e24d
HttpMarco <mirco.lindenau@gmx.de>
2024-01-26 00:09:52 +0100

    Complete hikari connection manager

--- evelon-sql-parent/src/main/java/dev/httpmarco/evelon/sql/parent/connection/HikariConnection.java
+++ evelon-sql-parent/src/main/java/dev/httpmarco/evelon/sql/parent/connection/HikariConnection.java
@@ -1,12 +1,41 @@
 package dev.httpmarco.evelon.sql.parent.connection;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import dev.httpmarco.evelon.common.layers.connection.RepositoryConnection;
+import lombok.SneakyThrows;
+
 import java.sql.Connection;
 
-public class HikariConnection {
+public class HikariConnection implements RepositoryConnection<Connection> {
 
-    public Connection getConnection() {
-        //todo
-        return null;
+    private HikariDataSource dataSource;
+    private final HikariConfig config;
+
+    public HikariConnection(HikariConfig config) {
+        this.config = config;
     }
 
+    @SneakyThrows
+    @Override
+    public Connection getConnection() {
+        return dataSource.getConnection();
+    }
+
+    @Override
+    public void close() {
+        if (dataSource != null && !dataSource.isClosed()) {
+            dataSource.close();
+        }
+    }
+
+    @Override
+    public void connect() {
+        this.dataSource = new HikariDataSource(this.config);
+    }
+
+    @Override
+    public boolean isConnected() {
+        return false;
+    }
 }
