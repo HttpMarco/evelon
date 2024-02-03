12e3a1f0fd21fc7d724f45f30231033bd62c65bc
HttpMarco <mirco.lindenau@gmx.de>
2024-02-03 23:16:57 +0100

    Add general simple api structure

--- evelon-common/src/main/java/dev/httpmarco/evelon/common/Evelon.java
+++ evelon-common/src/main/java/dev/httpmarco/evelon/common/Evelon.java
@@ -1,7 +1,22 @@
 package dev.httpmarco.evelon.common;
 
+import dev.httpmarco.evelon.common.layers.EvelonLayerPool;
+import lombok.Getter;
+import lombok.experimental.Accessors;
+
+@Getter
+@Accessors(fluent = true)
 public class Evelon {
 
+    private static Evelon instance;
 
+    private final EvelonLayerPool layerPool = new EvelonLayerPool();
+
+    public static Evelon getInstance() {
+        if (instance == null) {
+            return instance = new Evelon();
+        }
+        return instance;
+    }
 
 }
