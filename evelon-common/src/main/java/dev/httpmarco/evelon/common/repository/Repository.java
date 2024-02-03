21c070f32d62c157e9d13b0396774040e66cc5f4
HttpMarco <mirco.lindenau@gmx.de>
2024-02-03 23:17:17 +0100

    Add current layers on Repository

--- evelon-common/src/main/java/dev/httpmarco/evelon/common/repository/Repository.java
+++ evelon-common/src/main/java/dev/httpmarco/evelon/common/repository/Repository.java
@@ -1,9 +1,12 @@
 package dev.httpmarco.evelon.common.repository;
 
+import dev.httpmarco.evelon.common.layers.EvelonLayer;
 import dev.httpmarco.evelon.common.repository.clazz.RepositoryClass;
 
 public interface Repository<T> {
 
     RepositoryClass<T> clazz();
+
+    void addLayer(EvelonLayer<?> layer);
 
 }
--- evelon-common/src/main/java/dev/httpmarco/evelon/common/repository/RepositoryImpl.java
+++ evelon-common/src/main/java/dev/httpmarco/evelon/common/repository/RepositoryImpl.java
@@ -1,18 +1,27 @@
 package dev.httpmarco.evelon.common.repository;
 
+import dev.httpmarco.evelon.common.layers.EvelonLayer;
 import dev.httpmarco.evelon.common.repository.clazz.RepositoryClass;
 import dev.httpmarco.evelon.common.repository.clazz.RepositoryClassImpl;
 import lombok.Getter;
 import lombok.experimental.Accessors;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Accessors(fluent = true)
 public class RepositoryImpl<T> implements Repository<T> {
 
     private final RepositoryClass<T> clazz;
+    private final List<EvelonLayer<?>> currentLayers = new ArrayList<>();
 
     public RepositoryImpl(Class<T> clazz) {
         this.clazz = new RepositoryClassImpl<>(clazz);
     }
 
+    @Override
+    public void addLayer(EvelonLayer<?> layer) {
+        currentLayers.add(layer);
+    }
 }
