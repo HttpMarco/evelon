4d84a11aadfea58da3fad00edc901178a705ab35
HttpMarco <mirco.lindenau@gmx.de>
2024-02-03 09:09:44 +0100

    Add local storage on builder

--- evelon-common/src/main/java/dev/httpmarco/evelon/common/repository/RepositoryBuilder.java
+++ evelon-common/src/main/java/dev/httpmarco/evelon/common/repository/RepositoryBuilder.java
@@ -1,5 +1,6 @@
 package dev.httpmarco.evelon.common.repository;
 
+import dev.httpmarco.evelon.common.local.LocalCacheRepositoryImpl;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 
@@ -8,11 +9,21 @@ public final class RepositoryBuilder<T> {
 
     private final Class<T> clazz;
 
+    // current repository settings
+    private boolean useLocalStorage = false;
+
     public static <R> RepositoryBuilder<R> of(Class<R> clazz) {
         return new RepositoryBuilder<>(clazz);
     }
 
+    public RepositoryBuilder<T> withLocalStorage() {
+        this.useLocalStorage = true;
+        return this;
+    }
+
     public Repository<T> build() {
-        return new RepositoryImpl<>(clazz);
+        var repository = useLocalStorage ? new LocalCacheRepositoryImpl<>(clazz) : new RepositoryImpl<>(clazz);
+
+        return repository;
     }
 }
