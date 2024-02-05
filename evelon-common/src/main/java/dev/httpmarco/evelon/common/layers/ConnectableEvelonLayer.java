1f1c3e7cd7e967c2594f8e3939450829d5dcdfe2
HttpMarco <mirco.lindenau@gmx.de>
2024-02-05 19:36:07 +0100

    Add missing generic

--- evelon-common/src/main/java/dev/httpmarco/evelon/common/layers/ConnectableEvelonLayer.java
+++ evelon-common/src/main/java/dev/httpmarco/evelon/common/layers/ConnectableEvelonLayer.java
@@ -1,9 +1,10 @@
 package dev.httpmarco.evelon.common.layers;
 
+import dev.httpmarco.evelon.common.credentials.Credentials;
 import dev.httpmarco.evelon.common.layers.connection.EvelonLayerConnection;
 
-public interface ConnectableEvelonLayer<T, R> extends EvelonModelLayer<T> {
+public interface ConnectableEvelonLayer<T, R extends Credentials, C> extends EvelonModelLayer<T> {
 
-    EvelonLayerConnection<R> connection();
+    EvelonLayerConnection<R, C> connection();
 
 }
