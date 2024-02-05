ed131fea7e05d204dadcbe1aff63e98060b07aea
HttpMarco <mirco.lindenau@gmx.de>
2024-02-05 19:41:56 +0100

    Complete evelon credentials config

--- evelon-common/src/main/java/dev/httpmarco/evelon/common/Evelon.java
+++ evelon-common/src/main/java/dev/httpmarco/evelon/common/Evelon.java
@@ -1,5 +1,7 @@
 package dev.httpmarco.evelon.common;
 
+import dev.httpmarco.evelon.common.credentials.Credentials;
+import dev.httpmarco.evelon.common.credentials.CredentialsConfig;
 import dev.httpmarco.evelon.common.layers.EvelonLayerPool;
 import lombok.Getter;
 import lombok.experimental.Accessors;
@@ -11,6 +13,9 @@ public class Evelon {
     private static Evelon instance;
 
     private final EvelonLayerPool layerPool = new EvelonLayerPool();
+
+    // todo dynamic
+    private final CredentialsConfig credentialsConfig = new CredentialsConfig();
 
     public static Evelon getInstance() {
         if (instance == null) {
--- evelon-common/src/main/java/dev/httpmarco/evelon/common/credentials/CredentialsConfig.java
+++ evelon-common/src/main/java/dev/httpmarco/evelon/common/credentials/CredentialsConfig.java
@@ -1,5 +1,7 @@
 package dev.httpmarco.evelon.common.credentials;
 
+import dev.httpmarco.evelon.common.layers.EvelonLayer;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,4 +9,7 @@ public class CredentialsConfig {
 
     private final List<Credentials> credentials = new ArrayList<>();
 
+    public Credentials credentials(EvelonLayer<?> evelonLayer) {
+        return credentials.stream().filter(it -> it.id().equals(evelonLayer.id())).findFirst().orElseThrow();
+    }
 }
