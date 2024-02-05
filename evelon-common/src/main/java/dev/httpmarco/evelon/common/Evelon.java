1784a3b8f1456629ebc748785408885b0a9dce55
HttpMarco <mirco.lindenau@gmx.de>
2024-02-05 21:42:04 +0100

    Complete credentials reading

--- evelon-common/src/main/java/dev/httpmarco/evelon/common/Evelon.java
+++ evelon-common/src/main/java/dev/httpmarco/evelon/common/Evelon.java
@@ -1,8 +1,8 @@
 package dev.httpmarco.evelon.common;
 
-import dev.httpmarco.evelon.common.credentials.Credentials;
 import dev.httpmarco.evelon.common.credentials.CredentialsConfig;
 import dev.httpmarco.evelon.common.layers.EvelonLayerPool;
+import dev.httpmarco.osgon.configuration.ConfigHelper;
 import lombok.Getter;
 import lombok.experimental.Accessors;
 
@@ -14,8 +14,11 @@ public class Evelon {
 
     private final EvelonLayerPool layerPool = new EvelonLayerPool();
 
-    // todo dynamic
-    private final CredentialsConfig credentialsConfig = new CredentialsConfig();
+    private final CredentialsConfig credentialsConfig;
+
+    public Evelon() {
+        credentialsConfig = ConfigHelper.getConfigOrCreate("credentials.json", CredentialsConfig.class, new CredentialsConfig());
+    }
 
     public static Evelon getInstance() {
         if (instance == null) {
