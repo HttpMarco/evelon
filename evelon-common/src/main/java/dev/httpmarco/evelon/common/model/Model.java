3b977cafdcf933a611a1c535041252d19b705a15
HttpMarco <mirco.lindenau@gmx.de>
2024-02-05 11:52:37 +0100

    Add stages for model

--- evelon-common/src/main/java/dev/httpmarco/evelon/common/model/Model.java
+++ evelon-common/src/main/java/dev/httpmarco/evelon/common/model/Model.java
@@ -2,7 +2,11 @@ package dev.httpmarco.evelon.common.model;
 
 import dev.httpmarco.evelon.common.repository.RepositoryField;
 
+import java.util.List;
+
 public interface Model {
+
+    List<Stage> stages();
 
     Stage findStage(RepositoryField field);
 
