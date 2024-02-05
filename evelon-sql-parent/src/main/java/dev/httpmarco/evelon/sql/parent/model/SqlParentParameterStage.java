2eaa57dba5347c25305584c88835d8bd07375b73
HttpMarco <mirco.lindenau@gmx.de>
2024-02-05 11:08:37 +0100

    Generate sql parameter for sql parent

---
+++ evelon-sql-parent/src/main/java/dev/httpmarco/evelon/sql/parent/model/SqlParentParameterStage.java
@@ -0,0 +1,11 @@
+package dev.httpmarco.evelon.sql.parent.model;
+
+import dev.httpmarco.evelon.common.model.elements.AbstractSimpleParameterStage;
+
+public class SqlParentParameterStage extends AbstractSimpleParameterStage {
+
+    @Override
+    public Object serializeElement(Object element) {
+        return null;
+    }
+}
