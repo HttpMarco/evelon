d3bcf292ee42efda59cde6589d797b2f3bf6131f
HttpMarco <mirco.lindenau@gmx.de>
2024-02-05 18:25:06 +0100

    Implement default credentials logic

---
+++ evelon-common/src/main/java/dev/httpmarco/evelon/common/credentials/Credentials.java
@@ -0,0 +1,7 @@
+package dev.httpmarco.evelon.common.credentials;
+
+public interface Credentials {
+
+    String id();
+
+}
---
+++ evelon-common/src/main/java/dev/httpmarco/evelon/common/credentials/CredentialsConfig.java
@@ -0,0 +1,10 @@
+package dev.httpmarco.evelon.common.credentials;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CredentialsConfig {
+
+    private final List<Credentials> credentials = new ArrayList<>();
+
+}
