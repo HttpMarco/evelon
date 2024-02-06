7f40543f1d09e1f5b20406e64c867a47e672be5c
HttxMarco <mircolindenau04@gmail.com>
2024-02-06 08:52:30 +0100

    Add gson to test lib path

--- evelon-common/build.gradle.kts
+++ evelon-common/build.gradle.kts
@@ -1,6 +1,7 @@
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.2"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation(libs.gson)
 }
 
 tasks.test {
