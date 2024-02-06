692abbe6112cb2919e66b365a7e1dd61939de3d8
HttxMarco <mircolindenau04@gmail.com>
2024-02-06 08:56:41 +0100

    Start coding on default tests

--- evelon-common/src/test/java/dev/httpmarco/evelon/test/layers/SimpleRepositoryInsertTest.java
+++ evelon-common/src/test/java/dev/httpmarco/evelon/test/layers/SimpleRepositoryInsertTest.java
@@ -1,16 +1,24 @@
 package dev.httpmarco.evelon.test.layers;
 
+import dev.httpmarco.evelon.common.Evelon;
 import dev.httpmarco.evelon.common.repository.Repository;
 import dev.httpmarco.evelon.common.repository.RepositoryBuilder;
 import dev.httpmarco.evelon.test.TestRepository;
+import org.junit.jupiter.api.Test;
 
 public class SimpleRepositoryInsertTest {
 
-    public SimpleRepositoryInsertTest() {
+    @Test
+    public void SimpleRepositoryInsertTest() {
+
+        Evelon.getInstance();
 
         Repository<TestRepository> repository = RepositoryBuilder.of(TestRepository.class)
                 .withLocalStorage()
                 .build();
+
+
+
 
 
 
