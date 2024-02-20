5236c22b37617799c2f3f7553964a6f329c0530a
Mirco Lindenau <mirco.lindenau@gmx.de>
2024-02-20 23:50:17 +0100

    Update tests

--- evelon-common/src/test/java/dev/httpmarco/evelon/test/layers/SimpleRepositoryInsertTest.java
+++ evelon-common/src/test/java/dev/httpmarco/evelon/test/layers/SimpleRepositoryInsertTest.java
@@ -1,6 +1,5 @@
 package dev.httpmarco.evelon.test.layers;
 
-import dev.httpmarco.evelon.common.Evelon;
 import dev.httpmarco.evelon.common.repository.Repository;
 import dev.httpmarco.evelon.common.repository.RepositoryBuilder;
 import dev.httpmarco.evelon.test.TestRepository;
@@ -10,8 +9,6 @@ public class SimpleRepositoryInsertTest {
 
     @Test
     public void SimpleRepositoryInsertTest() {
-
-        Evelon.getInstance();
 
         Repository<TestRepository> repository = RepositoryBuilder.of(TestRepository.class)
                 .withLocalStorage()
