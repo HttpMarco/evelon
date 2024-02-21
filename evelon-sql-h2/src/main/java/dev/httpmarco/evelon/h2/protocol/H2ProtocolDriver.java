6047d575af349c537625755b623ed03d010569f7
Mirco Lindenau <mirco.lindenau@gmx.de>
2024-02-21 21:39:38 +0100

    Add jdbc string on driver

--- evelon-sql-h2/src/main/java/dev/httpmarco/evelon/h2/protocol/H2ProtocolDriver.java
+++ evelon-sql-h2/src/main/java/dev/httpmarco/evelon/h2/protocol/H2ProtocolDriver.java
@@ -1,15 +1,23 @@
 package dev.httpmarco.evelon.h2.protocol;
 
 import dev.httpmarco.evelon.sql.parent.layer.ProtocolDriver;
+import lombok.AllArgsConstructor;
 import org.h2.Driver;
 
+import java.nio.file.Path;
+
+@AllArgsConstructor
 public class H2ProtocolDriver implements ProtocolDriver {
+
+    private Path path;
 
     @Override
     public void onInitialize() {
         Driver.load();
+    }
 
-
-
+    @Override
+    public String jdbcString() {
+        return "h2:" + path.toAbsolutePath();
     }
 }
--- evelon-sql-parent/src/main/java/dev/httpmarco/evelon/sql/parent/layer/ProtocolDriver.java
+++ evelon-sql-parent/src/main/java/dev/httpmarco/evelon/sql/parent/layer/ProtocolDriver.java
@@ -4,4 +4,6 @@ public interface ProtocolDriver {
 
     void onInitialize();
 
+    String jdbcString();
+
 }
