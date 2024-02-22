04d17a4bcbea8422972e18c0ad495cf0957d44bf
HttxMarco <mircolindenau04@gmail.com>
2024-02-22 13:17:45 +0100

    Add evelon logger

--- evelon-common/build.gradle.kts
+++ evelon-common/build.gradle.kts
@@ -1,3 +1,3 @@
 dependencies {
-
+    api(libs.bundles.logger)
 }
--- evelon-common/src/main/java/dev/httpmarco/evelon/common/Evelon.java
+++ evelon-common/src/main/java/dev/httpmarco/evelon/common/Evelon.java
@@ -4,10 +4,14 @@ import dev.httpmarco.evelon.common.credentials.CredentialsService;
 import dev.httpmarco.evelon.common.layers.EvelonLayerPool;
 import lombok.Getter;
 import lombok.experimental.Accessors;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Getter
 @Accessors(fluent = true)
 public class Evelon {
+
+    public static Logger LOGGER = LoggerFactory.getLogger(Evelon.class);
 
     @Getter
     private final static Evelon instance = new Evelon();
