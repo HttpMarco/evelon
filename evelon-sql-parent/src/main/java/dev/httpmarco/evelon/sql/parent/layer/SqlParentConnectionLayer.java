1afb513c39ac36eb2f65efcad0c8c7923a3fc2eb
HttpMarco <mirco.lindenau@gmx.de>
2024-02-27 18:27:40 +0100

    Add todos

--- evelon-sql-parent/src/main/java/dev/httpmarco/evelon/sql/parent/layer/SqlParentConnectionLayer.java
+++ evelon-sql-parent/src/main/java/dev/httpmarco/evelon/sql/parent/layer/SqlParentConnectionLayer.java
@@ -48,111 +48,126 @@ public abstract class SqlParentConnectionLayer implements ConnectableEvelonLayer
 
     @Override
     public void create(DataQuery<Object> query, Object value) {
-
+        //todo
     }
 
     @Override
     public void createIfNotExists(DataQuery<Object> query, Object value) {
-
+        //todo
     }
 
     @Override
     public void deleteAll(DataQuery<Object> query) {
-
+        //todo
     }
 
     @Override
     public void delete(DataQuery<Object> query, Object value) {
-
+        //todo
     }
 
     @Override
     public void update(DataQuery<Object> query, Object value) {
-
+        //todo
     }
 
     @Override
     public void updateIf(DataQuery<Object> query, Object value, Predicate<Object> predicate) {
-
+        //todo
     }
 
     @Override
     public void upsert(DataQuery<Object> query, Object value) {
-
+        //todo
     }
 
     @Override
     public List<Object> findAll(DataQuery<Object> query) {
+        //todo
         return null;
     }
 
     @Override
     public List<Object> findAll(DataQuery<Object> query, int limit) {
+        //todo
         return null;
     }
 
     @Override
     public Object find(DataQuery<Object> query) {
+        //todo
         return null;
     }
 
     @Override
     public boolean exists(DataQuery<Object> query) {
+        //todo
         return false;
     }
 
     @Override
     public long count(DataQuery<Object> query) {
+        //todo
         return 0;
     }
 
     @Override
     public long sum(DataQuery<Object> query, String id) {
+        //todo
         return 0;
     }
 
     @Override
     public double avg(DataQuery<Object> query, String id) {
+        //todo
         return 0;
     }
 
     @Override
     public List<Object> order(DataQuery<Object> query, String id, int limit, SortedOrder order) {
+        //todo
         return null;
     }
 
     @Override
     public <E> List<E> collect(DataQuery<Object> query, String id, Class<E> clazz) {
+        //todo
         return null;
     }
 
     @Override
     public <E> List<E> collect(DataQuery<Object> query, String id, int limit, Class<E> clazz) {
+        //todo
         return null;
     }
 
     @Override
     public <E> E collectSingle(DataQuery<Object> query, String id, Class<E> clazz) {
+        //todo
         return null;
     }
 
     @Override
     public Object max(DataQuery<Object> query, String id) {
+        //todo
         return null;
     }
 
     @Override
     public Object min(DataQuery<Object> query, String id) {
+        //todo
         return null;
     }
 
     @Override
     public LayerFilterHandler<?, ?> filterHandler() {
+        //todo
         return null;
     }
 
     @Override
     public Model model() {
+        //todo
         return null;
     }
 }
