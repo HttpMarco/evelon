88b0d7646b1565b98d6ca688835becd204ac5156
HttxMarco <mircolindenau04@gmail.com>
2024-02-28 09:30:19 +0100

    Add todos

--- evelon-sql-parent/src/main/java/dev/httpmarco/evelon/sql/parent/model/SqlParentParameterStage.java
+++ evelon-sql-parent/src/main/java/dev/httpmarco/evelon/sql/parent/model/SqlParentParameterStage.java
@@ -2,11 +2,19 @@ package dev.httpmarco.evelon.sql.parent.model;
 
 import dev.httpmarco.evelon.common.model.elements.AbstractSimpleParameterStage;
 import dev.httpmarco.evelon.sql.parent.sql.SqlQueryBuilder;
+import dev.httpmarco.evelon.sql.parent.types.SqlType;
 
-public final class SqlParentParameterStage extends AbstractSimpleParameterStage<SqlQueryBuilder> {
+public final class SqlParentParameterStage extends AbstractSimpleParameterStage<SqlQueryBuilder, SqlType> {
 
     @Override
     public Object serializeElement(Object element) {
+        //todo
+        return null;
+    }
+
+    @Override
+    public SqlType classBuilderType() {
+        //todo
         return null;
     }
 }
