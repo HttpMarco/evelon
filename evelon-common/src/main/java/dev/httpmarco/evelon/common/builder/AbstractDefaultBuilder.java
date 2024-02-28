f81fb08a4ca7b7cd7ccc030cd09164466aadf7b1
Mirco Lindenau <mirco.lindenau@gmx.de>
2024-02-28 22:18:49 +0100

    Add better builder controller

--- evelon-common/src/main/java/dev/httpmarco/evelon/common/builder/AbstractDefaultBuilder.java
+++ evelon-common/src/main/java/dev/httpmarco/evelon/common/builder/AbstractDefaultBuilder.java
@@ -5,6 +5,7 @@ import dev.httpmarco.evelon.common.repository.RepositoryField;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.experimental.Accessors;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,9 +13,17 @@ import java.util.List;
 @Accessors(fluent = true)
 @Getter
 @AllArgsConstructor
-public abstract class AbstractDefaultBuilder<T extends Builder> implements Builder {
+public abstract class AbstractDefaultBuilder<T extends Builder<T>> implements Builder<T> {
+
+    private String id;
 
     private final Model<T> model;
     private final List<RepositoryField> queryFields = new ArrayList<>();
 
+    private final T parent;
+    private final List<T> children = new ArrayList<>();
+
+    public @Nullable T parent() {
+        return this.parent;
+    }
 }
--- evelon-common/src/main/java/dev/httpmarco/evelon/common/builder/Builder.java
+++ evelon-common/src/main/java/dev/httpmarco/evelon/common/builder/Builder.java
@@ -1,14 +1,23 @@
 package dev.httpmarco.evelon.common.builder;
 
+import dev.httpmarco.evelon.common.repository.RepositoryField;
+
 /**
  * represent a builder of elements in a data query/update
  */
-public interface Builder {
+public interface Builder<T> {
 
     /**
      * Create a new builder for sub elements, list or maps
+     *
      * @return new sub builder
      */
-    Builder subBuilder();
+    Builder<T> subBuilder(String id);
+
+    /**
+     * Add a value in the current storage
+     * @param repositoryField value how added
+     */
+    Builder<T> withField(RepositoryField repositoryField);
 
 }
