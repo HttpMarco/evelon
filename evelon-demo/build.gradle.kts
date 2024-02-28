1736f7cee9849555defff49df901a07b5dffd0fc
HttxMarco <mircolindenau04@gmail.com>
2024-02-28 08:51:04 +0100

    Fix: Demo runtime

--- evelon-demo/build.gradle.kts
+++ evelon-demo/build.gradle.kts
@@ -1,6 +1,10 @@
 dependencies {
     implementation(project(":evelon-common"))
+    runtimeOnly(project(":evelon-common"))
 
     //sql
     implementation(project(":evelon-sql-h2"))
+    implementation(project(":evelon-sql-parent"))
+
+    runtimeOnly(libs.gson)
 }
