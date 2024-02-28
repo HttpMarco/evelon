a8bb64e469ef9a9ea7bed833d42ecade0b527721
HttxMarco <mircolindenau04@gmail.com>
2024-02-28 09:06:22 +0100

    Add cast method for classes to object classes

--- evelon-common/src/main/java/dev/httpmarco/evelon/common/repository/clazz/RepositoryClass.java
+++ evelon-common/src/main/java/dev/httpmarco/evelon/common/repository/clazz/RepositoryClass.java
@@ -1,7 +1,15 @@
 package dev.httpmarco.evelon.common.repository.clazz;
 
+import lombok.SneakyThrows;
+
 public interface RepositoryClass<T> {
 
     Class<T> clazz();
 
+    default RepositoryObjectClass<T> asObjectClass() {
+        if (this instanceof RepositoryObjectClass<T> objectClass) {
+            return objectClass;
+        }
+        throw new RuntimeException("RepositoryClass is not an object class: " + this.getClass().getSimpleName());
+    }
 }
