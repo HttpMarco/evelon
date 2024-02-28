c934bd1fbaaf64b30cd09e9bde0f7be25d680a64
HttxMarco <mircolindenau04@gmail.com>
2024-02-28 08:53:31 +0100

    Work on sql stages

--- evelon-common/src/main/java/dev/httpmarco/evelon/common/model/elements/AbstractSimpleParameterStage.java
+++ evelon-common/src/main/java/dev/httpmarco/evelon/common/model/elements/AbstractSimpleParameterStage.java
@@ -3,7 +3,7 @@ package dev.httpmarco.evelon.common.model.elements;
 import dev.httpmarco.evelon.common.model.ElementStage;
 import dev.httpmarco.osgan.utils.Utils;
 
-public abstract class AbstractSimpleParameterStage implements ElementStage<Object> {
+public abstract class AbstractSimpleParameterStage<B> implements ElementStage<Object, B> {
 
     @Override
     public boolean isElement(Class<?> type) {
--- evelon-sql-parent/src/main/java/dev/httpmarco/evelon/sql/parent/model/SqlParentParameterStage.java
+++ evelon-sql-parent/src/main/java/dev/httpmarco/evelon/sql/parent/model/SqlParentParameterStage.java
@@ -1,8 +1,9 @@
 package dev.httpmarco.evelon.sql.parent.model;
 
 import dev.httpmarco.evelon.common.model.elements.AbstractSimpleParameterStage;
+import dev.httpmarco.evelon.sql.parent.sql.SqlQueryBuilder;
 
-public final class SqlParentParameterStage extends AbstractSimpleParameterStage {
+public final class SqlParentParameterStage extends AbstractSimpleParameterStage<SqlQueryBuilder> {
 
     @Override
     public Object serializeElement(Object element) {
