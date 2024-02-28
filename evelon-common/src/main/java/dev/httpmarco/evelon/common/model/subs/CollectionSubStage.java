7fcdde2683ecf4b5db93067f4603b983f7f79f16
Mirco Lindenau <mirco.lindenau@gmx.de>
2024-02-28 19:55:03 +0100

    Start coding on list elements

--- evelon-common/src/main/java/dev/httpmarco/evelon/common/model/subs/CollectionSubStage.java
+++ evelon-common/src/main/java/dev/httpmarco/evelon/common/model/subs/CollectionSubStage.java
@@ -1,10 +1,37 @@
 package dev.httpmarco.evelon.common.model.subs;
 
 import dev.httpmarco.evelon.common.builder.Builder;
+import dev.httpmarco.evelon.common.model.ElementStage;
+import dev.httpmarco.evelon.common.model.Model;
+import dev.httpmarco.evelon.common.model.Stage;
 import dev.httpmarco.evelon.common.model.SubStage;
+import dev.httpmarco.evelon.common.repository.RepositoryField;
+import dev.httpmarco.evelon.common.repository.clazz.RepositoryObjectClass;
+import dev.httpmarco.evelon.common.utils.GenericReader;
+
 import java.util.Collection;
 
-public abstract class CollectionSubStage<R extends Builder> implements SubStage<R>  {
+public abstract class CollectionSubStage<R extends Builder> implements SubStage<R> {
+
+    @Override
+    public void initialize(String stageId, Model<?> model, RepositoryField ownField, RepositoryObjectClass<?> clazz, R queries) {
+        var collectionType = GenericReader.readGenericFromClass(ownField.field());
+
+        if (collectionType.length > 1) {
+            throw new IllegalStateException("Collection stage tpe has multiple elements. That is not a requirement of collections.");
+        }
+
+        var stage = model.findStage(collectionType[0]);
+        if (stage instanceof ElementStage<?, ?, ?> elementStage) {
+
+            // todo
+        } else if (stage instanceof SubStage<?> substage) {
+            // todo
+        } else {
+            new UnsupportedOperationException("This stage is not supported yet.");
+        }
+
+    }
 
     @Override
     public boolean isElement(Class<?> type) {
