556a41d026c95ab49915ba943ab1710fc51134f0
HttxMarco <mircolindenau04@gmail.com>
2024-02-28 08:51:22 +0100

    Add default state query generic

--- evelon-common/src/main/java/dev/httpmarco/evelon/common/model/Stage.java
+++ evelon-common/src/main/java/dev/httpmarco/evelon/common/model/Stage.java
@@ -1,7 +1,10 @@
 package dev.httpmarco.evelon.common.model;
 
-public interface Stage {
+public interface Stage<R> {
 
     boolean isElement(Class<?> type);
 
+    default SubStage<R> asSubStage() {
+        return (SubStage<R>) this;
+    }
 }
--- evelon-common/src/main/java/dev/httpmarco/evelon/common/model/SubStage.java
+++ evelon-common/src/main/java/dev/httpmarco/evelon/common/model/SubStage.java
@@ -5,7 +5,7 @@ import dev.httpmarco.evelon.common.repository.clazz.RepositoryObjectClass;
 /**
  * @param <R> response of initialize phase (String for query languages)
  */
-public interface SubStage<R> extends Stage {
+public interface SubStage<R> extends Stage<R> {
 
     void initialize(String stageId, RepositoryObjectClass<?> clazz, R queries);
 
