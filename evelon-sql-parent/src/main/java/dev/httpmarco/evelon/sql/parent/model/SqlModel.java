f71ebd591feec84d74ad1443a7bbfeef5ec653e9
HttxMarco <mircolindenau04@gmail.com>
2024-02-28 10:01:07 +0100

    Fix stage ordering

--- evelon-sql-parent/src/main/java/dev/httpmarco/evelon/sql/parent/model/SqlModel.java
+++ evelon-sql-parent/src/main/java/dev/httpmarco/evelon/sql/parent/model/SqlModel.java
@@ -1,7 +1,7 @@
 package dev.httpmarco.evelon.sql.parent.model;
 
 import dev.httpmarco.evelon.common.model.Model;
-import dev.httpmarco.evelon.sql.parent.sql.SqlQueryBuilder;
+import dev.httpmarco.evelon.sql.parent.SqlQueryBuilder;
 import lombok.Getter;
 import lombok.experimental.Accessors;
 
@@ -11,7 +11,7 @@ public final class SqlModel extends Model<SqlQueryBuilder> {
 
     @Override
     public void applyPlatformStages() {
-        stages().add(new SqlParentVirtualSubStage());
         stages().add(new SqlParentParameterStage());
+        stages().add(new SqlParentVirtualSubStage());
     }
 }
