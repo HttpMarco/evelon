a24828e2605359dae4ea59fabb78f3a6d77c4006
HttxMarco <mircolindenau04@gmail.com>
2024-03-05 15:14:37 +0100

    Add map demo tests

---
+++ evelon-demo/src/main/java/dev/httpmarco/evelon/demo/MapBothElementTestRepository.java
@@ -0,0 +1,21 @@
+package dev.httpmarco.evelon.demo;
+
+import dev.httpmarco.evelon.common.annotations.PrimaryKey;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.experimental.Accessors;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Getter
+@Accessors(fluent = true)
+@AllArgsConstructor
+public class MapBothElementTestRepository {
+
+    @PrimaryKey
+    private final String mapName;
+    private final int age;
+    private final Map<String, String> properties = new HashMap<>();
+
+}
--- evelon-demo/src/main/java/dev/httpmarco/evelon/demo/h2/H2HierachyRepositoryTest.java
+++ evelon-demo/src/main/java/dev/httpmarco/evelon/demo/h2/H2HierachyRepositoryTest.java
@@ -2,10 +2,9 @@ package dev.httpmarco.evelon.demo.h2;
 
 import dev.httpmarco.evelon.common.repository.RepositoryBuilder;
 import dev.httpmarco.evelon.demo.HierarchyTestRepository;
-import dev.httpmarco.evelon.demo.SimpleListTestRepository;
 import dev.httpmarco.evelon.h2.layer.H2SqlLayer;
 
-public class H2HierachyTestRepository {
+public class H2HierachyRepositoryTest {
 
     public static void main(String[] args) {
         var repository = RepositoryBuilder.of(HierarchyTestRepository.class).addAfter(H2SqlLayer.class).build();
