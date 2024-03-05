e8431962fd562c8cb27674cd73d9c4bc63c6df18
HttxMarco <mircolindenau04@gmail.com>
2024-03-05 11:29:58 +0100

    Add general builder linking

--- evelon-common/src/main/java/dev/httpmarco/evelon/common/builder/Builder.java
+++ evelon-common/src/main/java/dev/httpmarco/evelon/common/builder/Builder.java
@@ -1,10 +1,14 @@
 package dev.httpmarco.evelon.common.builder;
 
+import dev.httpmarco.evelon.common.repository.field.PrimaryRepositoryFieldImpl;
+
 import java.util.List;
 
 public interface Builder<T extends Builder<?, A>, A> {
 
     List<T> children();
+
+    void linkPrimaries(PrimaryRepositoryFieldImpl... fields);
 
     T subBuilder(String subId);
 
