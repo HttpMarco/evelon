4c35068597c4170516751861040571bac31a3eb0
HttxMarco <mircolindenau04@gmail.com>
2024-03-05 10:53:19 +0100

    Add name for class

--- evelon-common/src/main/java/dev/httpmarco/evelon/common/repository/clazz/RepositoryClassImpl.java
+++ evelon-common/src/main/java/dev/httpmarco/evelon/common/repository/clazz/RepositoryClassImpl.java
@@ -2,13 +2,22 @@ package dev.httpmarco.evelon.common.repository.clazz;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import lombok.experimental.Accessors;
 
 @Getter
 @Accessors(fluent = true)
+@RequiredArgsConstructor
 @AllArgsConstructor
 public class RepositoryClassImpl<C> implements RepositoryClass<C> {
 
     private final Class<C> clazz;
 
+    @Setter
+    private String name;
+
+    public String name() {
+        return name == null ? clazz.getSimpleName() : name;
+    }
 }
