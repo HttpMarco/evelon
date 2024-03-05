81e753d2ab7b4d0fbd457da945147228783e6898
HttxMarco <mircolindenau04@gmail.com>
2024-03-05 11:29:16 +0100

    Set class type on initializeWithMapping process

--- evelon-common/src/main/java/dev/httpmarco/evelon/common/model/subs/AbstractVirtualSubStage.java
+++ evelon-common/src/main/java/dev/httpmarco/evelon/common/model/subs/AbstractVirtualSubStage.java
@@ -4,8 +4,8 @@ import dev.httpmarco.evelon.common.builder.Builder;
 import dev.httpmarco.evelon.common.model.Model;
 import dev.httpmarco.evelon.common.model.SubStage;
 import dev.httpmarco.evelon.common.repository.RepositoryField;
-import dev.httpmarco.evelon.common.repository.clazz.RepositoryClass;
 import dev.httpmarco.evelon.common.repository.clazz.RepositoryObjectClass;
+import dev.httpmarco.evelon.common.repository.clazz.RepositoryObjectClassImpl;
 
 public abstract class AbstractVirtualSubStage<R extends Builder<?, ?>> implements SubStage<R> {
 
@@ -13,8 +13,7 @@ public abstract class AbstractVirtualSubStage<R extends Builder<?, ?>> implement
     public void initialize(String stageId, Model<?> model, RepositoryField ownField, RepositoryObjectClass<?> clazz, R queries) {
         for (var field : clazz.fields()) {
             if (field.stage(model) instanceof SubStage<?> subStage) {
-                // todo set clazz !important
-                subStage.initializeWithMapping(stageId + "_" + field.id(), model, field, null, queries.subBuilder(field.id()));
+                subStage.initializeWithMapping(stageId + "_" + field.id(), model, field, new RepositoryObjectClassImpl<>(field.fieldType()), queries.subBuilder(field.id()));
             } else {
                 initializeSubElement(queries, field);
             }
