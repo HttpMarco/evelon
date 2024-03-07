e7e6fd3c02430ff8a02f6ca0bf86671d37a96c66
HttxMarco <mircolindenau04@gmail.com>
2024-03-07 14:09:20 +0100

    Improve RepositoryFieldImpl

--- evelon-common/src/main/java/dev/httpmarco/evelon/common/repository/field/RepositoryFieldImpl.java
+++ evelon-common/src/main/java/dev/httpmarco/evelon/common/repository/field/RepositoryFieldImpl.java
@@ -1,48 +1,39 @@
 package dev.httpmarco.evelon.common.repository.field;
 
-import dev.httpmarco.evelon.common.builder.Builder;
-import dev.httpmarco.evelon.common.model.Model;
-import dev.httpmarco.evelon.common.model.Stage;
 import dev.httpmarco.evelon.common.repository.clazz.RepositoryClassImpl;
 import dev.httpmarco.evelon.common.repository.clazz.RepositoryObjectClass;
 import lombok.Getter;
 import lombok.experimental.Accessors;
 import dev.httpmarco.evelon.common.repository.clazz.RepositoryClass;
 import dev.httpmarco.evelon.common.repository.RepositoryField;
+import org.jetbrains.annotations.Nullable;
 
 import java.lang.reflect.Field;
 
 @Getter
 @Accessors(fluent = true)
-@SuppressWarnings("unchecked")
 public class RepositoryFieldImpl<T> implements RepositoryField<T> {
 
     private final Field field;
     private final String id;
     private final Class<T> fieldType;
-
     private final RepositoryClass<T> clazz;
     private final RepositoryObjectClass<?> parentClass;
 
+    @SuppressWarnings("unchecked")
     public RepositoryFieldImpl(Field field, RepositoryObjectClass<?> parentClass) {
-        this.field = field;
-        this.id = this.field.getName();
-        this.fieldType = (Class<T>) this.field.getType();
-        this.clazz = new RepositoryClassImpl<>(this.fieldType);
-        this.parentClass = parentClass;
+        this(field.getName(), field, (Class<T>) field.getType(), null, parentClass);
     }
 
-    public RepositoryFieldImpl(Class<T> fieldType, String id, RepositoryObjectClass<?> parentClass) {
-        this.field = null;
-        this.fieldType = fieldType;
+    public RepositoryFieldImpl(RepositoryClass<T> clazz, String id, RepositoryObjectClass<?> parentClass) {
+        this(id, null, clazz.clazz(), clazz, parentClass);
+    }
+
+    public RepositoryFieldImpl(String id, @Nullable Field field, Class<T> fieldType, @Nullable RepositoryClass<T> clazz, RepositoryObjectClass<?> parentClass) {
         this.id = id;
-
-        this.clazz = new RepositoryClassImpl<>(this.fieldType);
+        this.field = field;
+        this.fieldType = fieldType;
         this.parentClass = parentClass;
-    }
-
-    @Override
-    public <B extends Builder<B, ?>> Stage<T, B> stage(Model<B> model) {
-        return model.findStage(this.fieldType);
+        this.clazz = clazz != null ? clazz : new RepositoryClassImpl<>(this.fieldType);
     }
 }
