7ffe5cc49070836bfe3102dbd0bc112b6f21094f
HttxMarco <mircolindenau04@gmail.com>
2024-03-07 14:08:18 +0100

    Add stages direct on classes

--- evelon-common/src/main/java/dev/httpmarco/evelon/common/repository/clazz/RepositoryClassImpl.java
+++ evelon-common/src/main/java/dev/httpmarco/evelon/common/repository/clazz/RepositoryClassImpl.java
@@ -1,14 +1,30 @@
 package dev.httpmarco.evelon.common.repository.clazz;
 
+import dev.httpmarco.evelon.common.builder.Builder;
+import dev.httpmarco.evelon.common.model.Model;
+import dev.httpmarco.evelon.common.model.Stage;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.Accessors;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Getter
 @Accessors(fluent = true)
 @RequiredArgsConstructor
+@SuppressWarnings("unchecked")
 public class RepositoryClassImpl<T> implements RepositoryClass<T> {
 
     private final Class<T> clazz;
+    private final Map<Model<?>, Stage<?, ?>> stages = new HashMap<>();
 
+    public void newModel(Model<?> model) {
+        stages.put(model, model.findStage(clazz));
+    }
+
+    @Override
+    public <B extends Builder<B, ?>> Stage<T, B> stageOf(Model<B> model) {
+        return (Stage<T, B>) stages.get(model);
+    }
 }
