d575509cdb8838094cdb508387cf8fc3948362df
HttxMarco <mircolindenau04@gmail.com>
2024-03-07 10:07:03 +0100

    Build methode for cast workaround

--- evelon-common/src/main/java/dev/httpmarco/evelon/common/model/Stage.java
+++ evelon-common/src/main/java/dev/httpmarco/evelon/common/model/Stage.java
@@ -3,16 +3,23 @@ package dev.httpmarco.evelon.common.model;
 import dev.httpmarco.evelon.common.builder.Builder;
 
 /**
- *
  * @param <T> Class type
  * @param <B> Specific builder type
  */
-public interface Stage<T, B extends Builder<?, ?>> {
+public interface Stage<T, B extends Builder<B, ?>> {
 
     boolean isElement(Class<?> type);
 
+    default boolean isSubStage() {
+        return this instanceof SubStage<T, B>;
+    }
+
     default SubStage<T, B> asSubStage() {
         return (SubStage<T, B>) this;
+    }
+
+    default boolean isSubElementStage() {
+        return this instanceof ElementStage<T, ?, B>;
     }
 
     default ElementStage<T, ?, B> asElementStage() {
