14d9c1478b6a41b0a9c1fc17272a6d64b9137830
HttxMarco <mircolindenau04@gmail.com>
2024-03-07 07:47:06 +0100

    Add layers method on repository

--- evelon-common/src/main/java/dev/httpmarco/evelon/common/repository/Repository.java
+++ evelon-common/src/main/java/dev/httpmarco/evelon/common/repository/Repository.java
@@ -4,11 +4,15 @@ import dev.httpmarco.evelon.common.layers.EvelonLayer;
 import dev.httpmarco.evelon.common.query.Query;
 import dev.httpmarco.evelon.common.repository.clazz.RepositoryClass;
 
+import java.util.List;
+
 public interface Repository<T> {
 
     RepositoryClass<T> clazz();
 
     void addLayer(EvelonLayer<?> layer);
+
+    List<EvelonLayer<T>> layers();
 
     Query<T> query();
 
