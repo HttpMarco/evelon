b7e28d93b056bdfd412c4663d455a14e3b25cf11
HttxMarco <mircolindenau04@gmail.com>
2024-03-07 14:08:04 +0100

    Remove foreign object

--- evelon-common/src/main/java/dev/httpmarco/evelon/common/repository/field/PrimaryRepositoryFieldImpl.java
+++ evelon-common/src/main/java/dev/httpmarco/evelon/common/repository/field/PrimaryRepositoryFieldImpl.java
@@ -1,16 +1,17 @@
 package dev.httpmarco.evelon.common.repository.field;
 
+import dev.httpmarco.evelon.common.repository.clazz.RepositoryClass;
 import dev.httpmarco.evelon.common.repository.clazz.RepositoryObjectClass;
 
 import java.lang.reflect.Field;
 
-public final class PrimaryRepositoryFieldImpl<T> extends ForeignLinkingRepositoryFieldImpl<T> {
+public final class PrimaryRepositoryFieldImpl<T> extends RepositoryFieldImpl<T> {
 
     public PrimaryRepositoryFieldImpl(Field field, RepositoryObjectClass<?> parentClass) {
         super(field, parentClass);
     }
 
-    public PrimaryRepositoryFieldImpl(Class<T> fieldType, String id, RepositoryObjectClass<?> parentClass) {
-        super(fieldType, id, parentClass);
+    public PrimaryRepositoryFieldImpl(RepositoryClass<T> clazz, String id, RepositoryObjectClass<?> parentClass) {
+        super(clazz, id, parentClass);
     }
 }
