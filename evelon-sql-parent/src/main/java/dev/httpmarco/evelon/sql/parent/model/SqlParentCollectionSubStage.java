c17a202108de83cc57b9e0ad3df40467b2e01384
HttxMarco <mircolindenau04@gmail.com>
2024-03-07 07:45:19 +0100

    Small code improvements

--- evelon-sql-parent/src/main/java/dev/httpmarco/evelon/sql/parent/model/SqlParentCollectionSubStage.java
+++ evelon-sql-parent/src/main/java/dev/httpmarco/evelon/sql/parent/model/SqlParentCollectionSubStage.java
@@ -2,7 +2,6 @@ package dev.httpmarco.evelon.sql.parent.model;
 
 import dev.httpmarco.evelon.common.model.subs.CollectionSubStage;
 import dev.httpmarco.evelon.common.repository.RepositoryField;
-import dev.httpmarco.evelon.common.repository.clazz.RepositoryClass;
 import dev.httpmarco.evelon.sql.parent.builder.SqlQueryBuilder;
 import org.jetbrains.annotations.NotNull;
 
