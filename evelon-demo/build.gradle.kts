a2be9beb506805b19610e6f9ee81fcd291f57023
HttxMarco <mircolindenau04@gmail.com>
2024-03-07 12:35:28 +0100

    Move junit tests

--- evelon-demo/build.gradle.kts
+++ evelon-demo/build.gradle.kts
@@ -7,4 +7,16 @@ dependencies {
     implementation(project(":evelon-sql-parent"))
 
     runtimeOnly(libs.gson)
+
+    testImplementation(platform("org.junit:junit-bom:5.10.2"))
+
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.test {
+    useJUnitPlatform()
+    testLogging {
+        events("passed", "skipped", "failed")
+    }
 }
