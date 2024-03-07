e919321b907c05342154257510cc2619030aaaf6
HttxMarco <mircolindenau04@gmail.com>
2024-03-07 10:22:25 +0100

    Recode virtual sub stage

--- evelon-common/src/main/java/dev/httpmarco/evelon/common/model/subs/AbstractVirtualSubStage.java
+++ evelon-common/src/main/java/dev/httpmarco/evelon/common/model/subs/AbstractVirtualSubStage.java
@@ -1,34 +1,61 @@
 package dev.httpmarco.evelon.common.model.subs;
 
 import dev.httpmarco.evelon.common.builder.Builder;
+import dev.httpmarco.evelon.common.model.ElementStage;
 import dev.httpmarco.evelon.common.model.Model;
 import dev.httpmarco.evelon.common.model.SubStage;
 import dev.httpmarco.evelon.common.repository.RepositoryField;
 import dev.httpmarco.evelon.common.repository.clazz.RepositoryObjectClass;
-import dev.httpmarco.evelon.common.repository.clazz.RepositoryObjectClassImpl;
 
-public abstract class AbstractVirtualSubStage<R extends Builder<?, ?>> implements SubStage<R> {
+import java.util.function.Consumer;
+
+public abstract class AbstractVirtualSubStage<B extends Builder<B, ?>> implements SubStage<Object, B> {
 
     @Override
-    public void initialize(String stageId, Model<?> model, RepositoryField ownField, RepositoryObjectClass<?> clazz, R queries) {
+    public void initialize(String stageId, Model<B> model, RepositoryField<?> ownField, RepositoryObjectClass<?> clazz, B queries) {
         for (var field : clazz.fields()) {
-            if (field.stage(model) instanceof SubStage<?> subStage) {
-                subStage.initializeWithMapping(stageId + "_" + field.id(), model, field, new RepositoryObjectClassImpl<>(field.fieldType()), queries.subBuilder(field.id()));
-            } else {
-                initializeSubElement(queries, field);
-            }
+            permitOnStage(field, model,
+                    subStage -> subStage.initialize(
+                            stageId + "_" + field.id(),
+                            model,
+                            field,
+                            field.clazz().asObjectClass(),
+                            queries.subBuilder(field.id())
+                    ),
+                    elementStage -> initializeSubElement(queries, field)
+            );
         }
     }
 
     @Override
-    public void create(String stageId, Model<?> model, RepositoryField ownField, RepositoryObjectClass<?> clazz, R queries) {
-        // todo
+    public void create(String stageId, Model<B> model, RepositoryField<Object> ownField, RepositoryObjectClass<?> clazz, B queries) {
+        for (var field : clazz.fields()) {
+            permitOnStage(field, model,
+                    subStage -> {
+
+                    },
+                    elementStage -> {
+
+                    }
+            );
+        }
     }
 
-    public abstract void initializeSubElement(R query, RepositoryField field);
+    public abstract void initializeSubElement(B query, RepositoryField<?> field);
 
     @Override
     public boolean isElement(Class<?> type) {
         return true;
+    }
+
+    private <T> void permitOnStage(RepositoryField<T> field, Model<B> model, Consumer<SubStage<T, B>> subStageHandling, Consumer<ElementStage<T, ?, B>> elementStageHandling) {
+        var stage = field.stage(model);
+        if (stage.isSubStage()) {
+            subStageHandling.accept(stage.asSubStage());
+        } else if (stage.isElementStage()) {
+            elementStageHandling.accept(stage.asElementStage());
+        } else {
+            throw new RuntimeException("Unknown stage type: " + stage.getClass().getSimpleName());
+        }
     }
 }
