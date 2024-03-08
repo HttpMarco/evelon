6cff982c07d624c9bb00796a9813d2b022d626b3
HttpMarco <mirco.lindenau@gmx.de>
2024-03-08 10:35:58 +0100

    Set field method deprecated and add missing documentation

--- evelon-common/src/main/java/dev/httpmarco/evelon/common/repository/RepositoryField.java
+++ evelon-common/src/main/java/dev/httpmarco/evelon/common/repository/RepositoryField.java
@@ -1,10 +1,6 @@
 package dev.httpmarco.evelon.common.repository;
 
-import dev.httpmarco.evelon.common.builder.Builder;
-import dev.httpmarco.evelon.common.model.Model;
-import dev.httpmarco.evelon.common.model.Stage;
 import dev.httpmarco.evelon.common.repository.clazz.RepositoryClass;
-import dev.httpmarco.osgan.reflections.Reflections;
 
 import java.lang.reflect.Field;
 
@@ -16,7 +12,6 @@ public interface RepositoryField<T> {
 
     /**
      * Returns the name of the field, serving as its identifier within the repository.
-     *
      * @return The name of the field, serving as its identifier.
      */
     String id();
@@ -24,24 +19,40 @@ public interface RepositoryField<T> {
     /**
      * Returns the RepositoryClass to which the current RepositoryField belongs.
      * The RepositoryClass represents the class or entity to which the field belongs.
-     *
      * @return The RepositoryClass to which the RepositoryField belongs.
      */
     RepositoryClass<?> parentClass();
 
     /**
      * Returns the class of the field
-     *
      * @return The class of field
      */
     Class<T> fieldType();
 
+    /**
+     * Get the own class parameters
+     * @return the own class as repository class
+     */
     RepositoryClass<T> clazz();
 
+    /**
+     * Get not the normal repository class, but the java class
+     * @return the java basic field
+     */
+    @Deprecated
     Field field();
 
-    @SuppressWarnings("unchecked")
-    default T value(Object parent) {
-        return (T) Reflections.of(parent).value(field());
-    }
+    /**
+     * Get the main repository of class
+     * @return the stage of the field
+     */
+    Repository<?> repository();
+
+    /**
+     * Get a value of parent object
+     * @param parent object
+     * @return the field value
+     */
+    T value(Object parent);
+
 }
