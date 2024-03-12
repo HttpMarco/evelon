49c9580bb0fe05d2a7ea9115d0b7f873cf11f987
HttxMarco <mircolindenau04@gmail.com>
2024-03-12 14:47:25 +0100

    Fix: Order tests

--- evelon-demo/src/test/java/dev/httpmarco/evelon/demo/h2/H2SimpleRepositoryTest.java
+++ evelon-demo/src/test/java/dev/httpmarco/evelon/demo/h2/H2SimpleRepositoryTest.java
@@ -5,26 +5,26 @@ import dev.httpmarco.evelon.common.repository.Repository;
 import dev.httpmarco.evelon.common.repository.RepositoryBuilder;
 import dev.httpmarco.evelon.demo.HierarchyElement;
 import dev.httpmarco.evelon.demo.HierarchyTestRepository;
-import dev.httpmarco.evelon.demo.SimpleTestRepository;
+import dev.httpmarco.evelon.demo.SimpleModel;
 import dev.httpmarco.evelon.h2.layer.H2SqlLayer;
 import org.junit.jupiter.api.*;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public final class H2SimpleRepositoryTest {
 
     @Nested
+    @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
     @DisplayName("H2 - Simple Repository Test")
     class SimpleTest {
 
-        private static Repository<SimpleTestRepository> REPOSITORY;
-        private static SimpleTestRepository TEST_MODEL;
+        private static Repository<SimpleModel> REPOSITORY;
+        private static SimpleModel TEST_MODEL;
 
         @BeforeAll
         static void initialize() {
-            TEST_MODEL = new SimpleTestRepository(1, 200);
-            REPOSITORY = RepositoryBuilder.of(SimpleTestRepository.class).addAfter(H2SqlLayer.class).build();
+            TEST_MODEL = new SimpleModel(1, 200);
+            REPOSITORY = RepositoryBuilder.of(SimpleModel.class).addAfter(H2SqlLayer.class).build();
         }
 
         @Test
@@ -35,19 +35,24 @@ public final class H2SimpleRepositoryTest {
 
         @Test
         @Order(2)
-        void deleteAll() {
-            assertEquals(ResponseType.SUCCESS, REPOSITORY.query().deleteAll().response());
-        }
-
-        @Test
-        @Order(3)
         void exists() {
             assertTrue(REPOSITORY.query().exists());
         }
 
-        @Order(4)
+        @Test
+        @Order(3)
         void get() {
-            assertEquals(TEST_MODEL, REPOSITORY.query().findFirst());
+            var model = REPOSITORY.query().findFirst();
+
+            assertNotNull(model);
+            assertEquals(TEST_MODEL.username(), model.username());
+            assertEquals(TEST_MODEL.money(), model.money());
+        }
+
+        @Test
+        @Order(4)
+        void deleteAll() {
+            assertEquals(ResponseType.SUCCESS, REPOSITORY.query().deleteAll().response());
         }
     }
 
