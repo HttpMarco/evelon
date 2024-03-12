9e5a8c98c839e25ab7bc46e6f3b0dd0d99522512
HttxMarco <mircolindenau04@gmail.com>
2024-03-12 09:29:00 +0100

    Recode connection transmitter

--- evelon-sql-parent/src/main/java/dev/httpmarco/evelon/sql/parent/connection/HikariConnectionTransmitter.java
+++ evelon-sql-parent/src/main/java/dev/httpmarco/evelon/sql/parent/connection/HikariConnectionTransmitter.java
@@ -2,11 +2,13 @@ package dev.httpmarco.evelon.sql.parent.connection;
 
 import dev.httpmarco.evelon.common.Evelon;
 import dev.httpmarco.evelon.common.query.response.QueryResponse;
-import dev.httpmarco.evelon.common.query.response.ResponseType;
+import dev.httpmarco.evelon.common.query.response.ResponseResult;
+import dev.httpmarco.evelon.common.query.response.UpdateResponse;
 import lombok.AllArgsConstructor;
 import org.jetbrains.annotations.NotNull;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.Objects;
@@ -16,30 +18,34 @@ public final class HikariConnectionTransmitter {
 
     private HikariConnection hikariConnection;
 
-    public QueryResponse executeUpdate(final String query, List<Object> arguments) {
-        return this.transferPreparedStatement(PreparedStatement::executeUpdate, query, arguments.toArray());
+    public @NotNull UpdateResponse executeUpdate(final String query, @NotNull List<Object> arguments) {
+        return this.executeUpdate(query, arguments.toArray());
     }
 
-    public QueryResponse executeUpdate(final String query, Object... arguments) {
-        return this.transferPreparedStatement(PreparedStatement::executeUpdate, query, arguments);
+    public @NotNull UpdateResponse executeUpdate(final String query, Object... arguments) {
+        return this.transferPreparedStatement(it -> {
+            it.executeUpdate();
+            return new UpdateResponse();
+        }, query, arguments);
     }
 
 
-    public QueryResponse executeQuery(final String query, Object... arguments) {
-        return this.transferPreparedStatement(PreparedStatement::executeQuery, query, arguments);
+    public <T> @NotNull QueryResponse<T> executeQuery(final String query, HikariConnectionFunction<ResultSet, T> function, T defaultValue, Object... arguments) {
+        return this.transferPreparedStatement(it -> new QueryResponse<T>().result(function.apply(it.executeQuery())), query, arguments);
     }
 
-    public QueryResponse transferPreparedStatement(StatementTransmitter statementTransmitter, String query, Object @NotNull ... arguments) {
-        var response = QueryResponse.empty();
+    @SuppressWarnings("unchecked")
+    private <R extends ResponseResult> @NotNull R transferPreparedStatement(HikariConnectionFunction<PreparedStatement, R> function, String query, Object @NotNull ... arguments) {
+        var response = new ResponseResult();
         Evelon.LOGGER.info("Executing query: {}", query);
         try (var connection = hikariConnection.getConnection(); var statement = connection.prepareStatement(query)) {
             for (int i = 0; i < arguments.length; i++) {
                 statement.setString(i + 1, Objects.toString(arguments[i]));
             }
-            statementTransmitter.result(statement);
+            response.append(function.apply(statement));
         } catch (SQLException exception) {
             response.appendError(exception.getMessage());
         }
-        return response;
+        return (R) response;
     }
 }
