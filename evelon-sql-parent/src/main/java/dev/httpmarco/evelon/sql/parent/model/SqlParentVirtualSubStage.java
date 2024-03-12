1dd144a24add3b356e50fb174363562c822775a5
HttxMarco <mircolindenau04@gmail.com>
2024-03-12 14:47:10 +0100

    Add transformer for sql virtual sub stage

--- evelon-sql-parent/src/main/java/dev/httpmarco/evelon/sql/parent/model/SqlParentVirtualSubStage.java
+++ evelon-sql-parent/src/main/java/dev/httpmarco/evelon/sql/parent/model/SqlParentVirtualSubStage.java
@@ -11,4 +11,17 @@ public final class SqlParentVirtualSubStage extends AbstractVirtualSubStage<SqlQ
     public void appendParameter(@NotNull SqlQueryBuilder query, RepositoryField<?> field) {
         query.addRowType(field);
     }
+
+    @Override
+    public void transformData(SqlQueryBuilder builder, RepositoryField<?>... requiredFields) {
+        builder.query(i -> {
+            while (i.next()) {
+                for (var field : requiredFields) {
+                    builder.appendValue(field.id(), i.getObject(field.id()));
+                }
+            }
+            return null;
+        }, // todo: find a better way (return)
+        null);
+    }
 }
