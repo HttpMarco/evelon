3c4d56d59216ea700f14012f9c138cea3cdebd92
HttxMarco <mircolindenau04@gmail.com>
2024-03-12 13:18:33 +0100

    Add documentation and rename queries to builder

--- evelon-common/src/main/java/dev/httpmarco/evelon/common/model/SubStage.java
+++ evelon-common/src/main/java/dev/httpmarco/evelon/common/model/SubStage.java
@@ -7,14 +7,31 @@ import dev.httpmarco.evelon.common.repository.clazz.RepositoryObjectClass;
 
 public interface SubStage<T, B extends Builder<B, ?, ?>> extends Stage<T, B> {
 
-    void initialize(Repository<?> repository, String stageId, Model<B> model, RepositoryField<?> ownField, RepositoryObjectClass<?> clazz, B queries);
+    /**
+     * Initialize the repository values
+     * @param repository repository
+     * @param stageId parent collect id
+     * @param model model
+     * @param ownField self field in parent class
+     * @param clazz parent class
+     * @param builder builder
+     */
+    void initialize(Repository<?> repository, String stageId, Model<B> model, RepositoryField<?> ownField, RepositoryObjectClass<?> clazz, B builder);
 
+    /**
+     * Save a value in the database
+     * @param value of element
+     * @param stageId parent collect id
+     * @param model model
+     * @param ownField self field in parent class
+     * @param clazz parent class
+     * @param queries builder
+     */
     void create(T value, String stageId, Model<B> model, RepositoryField<?> ownField, RepositoryObjectClass<?> clazz, B queries);
 
     @SuppressWarnings("unchecked")
     @Deprecated
-    default void createMapping(Object value, String stageId, Model<B> model, RepositoryField<?> ownField, RepositoryObjectClass<?> clazz, B queries) {
-        this.create((T) value, stageId, model, ownField, clazz, queries);
+    default void createMapping(Object value, String stageId, Model<B> model, RepositoryField<?> ownField, RepositoryObjectClass<?> clazz, B builder) {
+        this.create((T) value, stageId, model, ownField, clazz, builder);
     }
-
 }
