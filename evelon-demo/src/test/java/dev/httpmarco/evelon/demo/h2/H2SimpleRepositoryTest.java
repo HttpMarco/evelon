6f418058dbc3080ead655773626e482eb1156a4b
HttxMarco <mircolindenau04@gmail.com>
2024-03-12 10:45:59 +0100

    Make tests easier

--- evelon-demo/src/test/java/dev/httpmarco/evelon/demo/h2/H2SimpleRepositoryTest.java
+++ evelon-demo/src/test/java/dev/httpmarco/evelon/demo/h2/H2SimpleRepositoryTest.java
@@ -10,6 +10,7 @@ import dev.httpmarco.evelon.h2.layer.H2SqlLayer;
 import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public final class H2SimpleRepositoryTest {
 
@@ -17,21 +18,36 @@ public final class H2SimpleRepositoryTest {
     @DisplayName("H2 - Simple Repository Test")
     class SimpleTest {
 
-        private static Repository<SimpleTestRepository> repository;
+        private static Repository<SimpleTestRepository> REPOSITORY;
+        private static SimpleTestRepository TEST_MODEL;
 
         @BeforeAll
         static void initialize() {
-            repository = RepositoryBuilder.of(SimpleTestRepository.class).addAfter(H2SqlLayer.class).build();
+            TEST_MODEL = new SimpleTestRepository(1, 200);
+            REPOSITORY = RepositoryBuilder.of(SimpleTestRepository.class).addAfter(H2SqlLayer.class).build();
         }
 
         @Test
+        @Order(1)
         void creation() {
-            assertEquals(ResponseType.SUCCESS, repository.query().create(new SimpleTestRepository(1, 200)).response());
+            assertEquals(ResponseType.SUCCESS, REPOSITORY.query().create(TEST_MODEL).response());
         }
 
         @Test
+        @Order(2)
         void deleteAll() {
-            assertEquals(ResponseType.SUCCESS, repository.query().deleteAll().response());
+            assertEquals(ResponseType.SUCCESS, REPOSITORY.query().deleteAll().response());
+        }
+
+        @Test
+        @Order(3)
+        void exists() {
+            assertTrue(REPOSITORY.query().exists());
+        }
+
+        @Order(4)
+        void get() {
+            assertEquals(TEST_MODEL, REPOSITORY.query().findFirst());
         }
     }
 
