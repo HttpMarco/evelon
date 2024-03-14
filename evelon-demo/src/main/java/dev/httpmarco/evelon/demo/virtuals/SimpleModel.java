8f0a46017429db69766b262ef8313e90b0353e82
HttxMarco <mircolindenau04@gmail.com>
2024-03-14 11:13:53 +0100

    Add equals and hashcode method for objects

--- evelon-demo/src/main/java/dev/httpmarco/evelon/demo/virtuals/SimpleModel.java
+++ evelon-demo/src/main/java/dev/httpmarco/evelon/demo/virtuals/SimpleModel.java
@@ -2,12 +2,16 @@ package dev.httpmarco.evelon.demo.virtuals;
 
 import dev.httpmarco.evelon.common.annotations.PrimaryKey;
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.experimental.Accessors;
+
+import java.util.Objects;
 
 @Accessors(fluent = true)
 @Getter
 @AllArgsConstructor
+@EqualsAndHashCode
 public final class SimpleModel {
 
     @PrimaryKey
