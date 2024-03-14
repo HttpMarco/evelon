12a0c0cafae7579098db6db50607acfc1755f13a
HttxMarco <mircolindenau04@gmail.com>
2024-03-14 09:41:21 +0100

    Add nullable annotation

--- evelon-common/src/main/java/dev/httpmarco/evelon/common/filtering/Filter.java
+++ evelon-common/src/main/java/dev/httpmarco/evelon/common/filtering/Filter.java
@@ -4,6 +4,7 @@ import dev.httpmarco.evelon.common.repository.Repository;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.experimental.Accessors;
+import org.jetbrains.annotations.Nullable;
 
 @AllArgsConstructor
 @Accessors(fluent = true)
@@ -13,7 +14,7 @@ public abstract class Filter<T, R> {
     private String id;
     private R value;
 
-    public abstract T filter(Repository<?> repository, R requiredType);
+    public abstract T filter(Repository<?> repository, @Nullable R requiredType);
 
     public boolean requirementCheck(Class<?> clazz) {
         return true;
