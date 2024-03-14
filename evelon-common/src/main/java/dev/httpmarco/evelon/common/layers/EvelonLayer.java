99721928eaed75fadd11d1886a26cfc906207b51
HttxMarco <mircolindenau04@gmail.com>
2024-03-14 09:03:08 +0100

    Remove DataQuery and move to query

--- evelon-common/src/main/java/dev/httpmarco/evelon/common/layers/EvelonLayer.java
+++ evelon-common/src/main/java/dev/httpmarco/evelon/common/layers/EvelonLayer.java
@@ -1,8 +1,8 @@
 package dev.httpmarco.evelon.common.layers;
 
 import dev.httpmarco.evelon.common.filtering.LayerFilterHandler;
+import dev.httpmarco.evelon.common.query.Query;
 import dev.httpmarco.evelon.common.query.SortedOrder;
-import dev.httpmarco.evelon.common.query.intern.DataQuery;
 import dev.httpmarco.evelon.common.query.response.QueryResponse;
 import dev.httpmarco.evelon.common.query.response.UpdateResponse;
 
@@ -23,7 +23,7 @@ public interface EvelonLayer<T> extends EvelonLayerSession {
      * @param query The DataQuery object representing the query for creating the data entry.
      * @param value The value of type T that will be associated with the data entry.
      */
-    UpdateResponse create(DataQuery<T> query, T value);
+    UpdateResponse create(Query<T> query, T value);
 
     /**
      * Creates a new entry in the data store if it does not already exist.
@@ -31,14 +31,14 @@ public interface EvelonLayer<T> extends EvelonLayerSession {
      * @param query the {@code DataQuery} used to check for the existence of the entry
      * @param value the value to be stored if the entry does not exist
      */
-    void createIfNotExists(DataQuery<T> query, T value);
+    void createIfNotExists(Query<T> query, T value);
 
     /**
      * Deletes data entries based on the specified query.
      *
      * @param query The DataQuery object representing the query for deleting data entries.
      */
-    UpdateResponse deleteAll(DataQuery<T> query);
+    UpdateResponse deleteAll(Query<T> query);
 
     /**
      * Deletes data entries based on the specified query.
@@ -46,7 +46,7 @@ public interface EvelonLayer<T> extends EvelonLayerSession {
      * @param query The DataQuery object representing the query for deleting data entries.
      * @param value The value of type T that will be associated with the data entry.
      */
-    void delete(DataQuery<T> query, T value);
+    void delete(Query<T> query, T value);
 
     /**
      * Updates existing data entries based on the specified query and new value.
@@ -54,7 +54,7 @@ public interface EvelonLayer<T> extends EvelonLayerSession {
      * @param query The DataQuery object representing the query for updating data entries.
      * @param value The new value of type T that will replace the existing value.
      */
-    void update(DataQuery<T> query, T value);
+    void update(Query<T> query, T value);
 
     /**
      * Updates existing data entries based on the specified query and new value.
@@ -63,7 +63,7 @@ public interface EvelonLayer<T> extends EvelonLayerSession {
      * @param value     The new value of type T that will replace the existing value.
      * @param predicate The value will only be updated if the specified predicate returns true.
      */
-    void updateIf(DataQuery<T> query, T value, Predicate<T> predicate);
+    void updateIf(Query<T> query, T value, Predicate<T> predicate);
 
     /**
      * Updates or inserts a new data entry based on the specified query and value.
@@ -71,7 +71,7 @@ public interface EvelonLayer<T> extends EvelonLayerSession {
      * @param query The DataQuery object representing the query for upserting data entries.
      * @param value The value of type T that will be associated with the data entry.
      */
-    void upsert(DataQuery<T> query, T value);
+    void upsert(Query<T> query, T value);
 
     /**
      * Finds all data entries based on the specified query.
@@ -79,7 +79,7 @@ public interface EvelonLayer<T> extends EvelonLayerSession {
      * @param query The DataQuery object representing the query for finding data entries.
      * @return A List containing all data entries that match the query.
      */
-    List<T> findAll(DataQuery<T> query);
+    List<T> findAll(Query<T> query);
 
     /**
      * Finds all data entries based on the specified query.
@@ -88,7 +88,7 @@ public interface EvelonLayer<T> extends EvelonLayerSession {
      * @param limit The maximum number of data entries to retrieve.
      * @return A List containing all data entries that match the query.
      */
-    List<T> findAll(DataQuery<T> query, int limit);
+    List<T> findAll(Query<T> query, int limit);
 
     /**
      * Finds a single data entry based on the specified query.
@@ -96,7 +96,7 @@ public interface EvelonLayer<T> extends EvelonLayerSession {
      * @param query The DataQuery object representing the query for finding a data entry.
      * @return The data entry that matches the query, or null if no match is found.
      */
-    QueryResponse<T> findFirst(DataQuery<T> query);
+    QueryResponse<T> findFirst(Query<T> query);
 
     /**
      * Checks if any data entries exist based on the specified query.
@@ -104,7 +104,7 @@ public interface EvelonLayer<T> extends EvelonLayerSession {
      * @param query The DataQuery object representing the query for checking the existence of data entries.
      * @return True if at least one data entry exists, otherwise false.
      */
-    QueryResponse<Boolean> exists(DataQuery<T> query);
+    QueryResponse<Boolean> exists(Query<T> query);
 
     /**
      * Counts the number of data entries based on the specified query.
@@ -112,7 +112,7 @@ public interface EvelonLayer<T> extends EvelonLayerSession {
      * @param query The DataQuery object representing the query for counting data entries.
      * @return The number of data entries that match the query.
      */
-    long count(DataQuery<T> query);
+    long count(Query<T> query);
 
     /**
      * Calculates the sum of a numeric property (specified by 'id') of data entries based on the specified query.
@@ -121,7 +121,7 @@ public interface EvelonLayer<T> extends EvelonLayerSession {
      * @param id    The identifier of the numeric property to be summed.
      * @return The sum of the specified numeric property for data entries that match the query.
      */
-    long sum(DataQuery<T> query, String id);
+    long sum(Query<T> query, String id);
 
     /**
      * Calculates the average of a numeric property (specified by 'id') of data entries based on the specified query.
@@ -130,7 +130,7 @@ public interface EvelonLayer<T> extends EvelonLayerSession {
      * @param id    The identifier of the numeric property for which the average is calculated.
      * @return The average of the specified numeric property for data entries that match the query.
      */
-    double avg(DataQuery<T> query, String id);
+    double avg(Query<T> query, String id);
 
     /**
      * Orders and retrieves a specified maximum number of data entries based on the specified query and sorting criteria.
@@ -141,17 +141,17 @@ public interface EvelonLayer<T> extends EvelonLayerSession {
      * @param order The sorting order (ascending or descending).
      * @return A List containing the ordered data entries up to the specified maximum.
      */
-    List<T> order(DataQuery<T> query, String id, int limit, SortedOrder order);
+    List<T> order(Query<T> query, String id, int limit, SortedOrder order);
 
-    <E> List<E> collect(DataQuery<T> query, String id, Class<E> clazz);
+    <E> List<E> collect(Query<T> query, String id, Class<E> clazz);
 
-    <E> List<E> collect(DataQuery<T> query, String id, int limit, Class<E> clazz);
+    <E> List<E> collect(Query<T> query, String id, int limit, Class<E> clazz);
 
-    <E> E collectSingle(DataQuery<T> query, String id, Class<E> clazz);
+    <E> E collectSingle(Query<T> query, String id, Class<E> clazz);
 
-    T max(DataQuery<T> query, String id);
+    T max(Query<T> query, String id);
 
-    T min(DataQuery<T> query, String id);
+    T min(Query<T> query, String id);
 
     /**
      * Gets the filter handler for this layer.
--- evelon-common/src/main/java/dev/httpmarco/evelon/common/query/Query.java
+++ evelon-common/src/main/java/dev/httpmarco/evelon/common/query/Query.java
@@ -1,15 +1,66 @@
 package dev.httpmarco.evelon.common.query;
 
+import dev.httpmarco.evelon.common.filtering.Filter;
+import dev.httpmarco.evelon.common.layers.EvelonLayer;
 import dev.httpmarco.evelon.common.query.response.UpdateResponse;
+import dev.httpmarco.evelon.common.repository.Repository;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.Accessors;
 
-public interface Query<T> {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
 
-    UpdateResponse create(T value);
+@RequiredArgsConstructor
+@Accessors(fluent = true)
+public class Query<T> {
 
-    UpdateResponse deleteAll();
+    @Getter
+    private final Repository<T> repository;
+    private final List<Filter<?, ?>> filters = new ArrayList<>();
 
-    T findFirst();
+    /**
+     * Create a specific value on all layers
+     * @param value new value
+     * @return the update result
+     */
+    public UpdateResponse create(T value) {
+        return this.alertLayers(layer -> layer.create(this, value));
+    }
 
-    boolean exists();
+    /**
+     * Delete all objects with filter accepting value (on all layers)
+     * @return the update result
+     */
+    public UpdateResponse deleteAll() {
+        return this.alertLayers(layer -> layer.deleteAll(this));
+    }
 
+    public T findFirst() {
+        for (var layer : repository.layers()) {
+            if (layer.exists(this).result()) {
+                return layer.findFirst(this).result();
+            }
+        }
+        return null;
+    }
+
+    public boolean exists() {
+        for (var layer : repository.layers()) {
+            if (layer.exists(this).result()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private UpdateResponse alertLayers(Function<EvelonLayer<T>, UpdateResponse> function) {
+        var response = new UpdateResponse();
+        for (var layer : repository.layers()) {
+            response.append(function.apply(layer));
+        }
+        return response.close();
+    }
 }
+
