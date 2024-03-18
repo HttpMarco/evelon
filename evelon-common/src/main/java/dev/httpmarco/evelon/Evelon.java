7e36d84bf2f34d007022b208af5f730835398a34
HttpMarco <mirco.lindenau@gmx.de>
2024-03-18 23:20:50 +0100

    Add logger and default services

--- evelon-common/src/main/java/dev/httpmarco/evelon/Evelon.java
+++ evelon-common/src/main/java/dev/httpmarco/evelon/Evelon.java
@@ -1,4 +1,26 @@
 package dev.httpmarco.evelon;
 
+import dev.httpmarco.evelon.stage.StageService;
+import lombok.Getter;
+import lombok.experimental.Accessors;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Getter
+@Accessors(fluent = true)
 public class Evelon {
+
+    // the single instance of evelon
+    @Getter @Accessors(fluent = true)
+    private static final Evelon instance = new Evelon();
+
+    // main logger of evelon
+    public static final Logger LOGGER = LoggerFactory.getLogger(Evelon.class);
+
+    // the single point of access to the stage service
+    private final StageService stageService = new StageService();
+
+
+
+
 }
