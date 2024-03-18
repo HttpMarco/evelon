e5f9321c8077fde56299b324e3497916aa4ea50a
HttpMarco <mirco.lindenau@gmx.de>
2024-03-18 23:21:08 +0100

    Implement ObjectType

---
+++ evelon-common/src/main/java/dev/httpmarco/evelon/stage/types/ObjectType.java
@@ -0,0 +1,18 @@
+package dev.httpmarco.evelon.stage.types;
+
+import dev.httpmarco.evelon.Evelon;
+import dev.httpmarco.evelon.stage.Stage;
+
+import java.util.Arrays;
+
+/**
+ * This class is a stage type that is used to represent a type that is an object.
+ */
+public class ObjectType extends Stage.Type {
+
+    @Override
+    public boolean isType(Class<?> clazz) {
+        // we only accept fields that are of a type that is a stage type
+        return Arrays.stream(clazz.getDeclaredFields()).allMatch(it -> Evelon.instance().stageService().typeOf(it.getType()) != null);
+    }
+}
