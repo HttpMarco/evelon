6f3d4dc080ae016bc62bdbf90d3aa0ff524c4de2
HttpMarco <mirco.lindenau@gmx.de>
2024-03-18 23:20:59 +0100

    Add javadoc

--- evelon-common/src/main/java/dev/httpmarco/evelon/repository/Repository.java
+++ evelon-common/src/main/java/dev/httpmarco/evelon/repository/Repository.java
@@ -1,4 +1,29 @@
 package dev.httpmarco.evelon.repository;
 
-public class Repository<T> {
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.experimental.Accessors;
+
+/**
+ * Repository class, which is used to store the data
+ * @param <T> the type of the data
+ */
+@AllArgsConstructor(access = AccessLevel.PACKAGE)
+@Getter @Accessors(fluent = true)
+public final class Repository<T> {
+
+    // main name of the repository. Can be modified by the @Repository annotation
+    private final String name;
+    private final RepositoryClass<T> clazz;
+    // todo list of current layers
+
+    /**
+     * Single point, which is used to create new builder of repo
+     * @return the new builder of repo
+     * @param <T> repo value type
+     */
+    public static <T> RepositoryBuilder<T> create(Class<T> clazz) {
+        return new RepositoryBuilder<>(clazz);
+    }
 }
--- evelon-common/src/main/java/dev/httpmarco/evelon/stage/types/ParameterType.java
+++ evelon-common/src/main/java/dev/httpmarco/evelon/stage/types/ParameterType.java
@@ -2,11 +2,16 @@ package dev.httpmarco.evelon.stage.types;
 
 import dev.httpmarco.evelon.stage.Stage;
 import dev.httpmarco.osgan.utils.Utils;
+import org.jetbrains.annotations.NotNull;
 
+/**
+ * This class is a stage type that is used to represent a type that is a parameter
+ * or a primitive type.
+ */
 public class ParameterType extends Stage.Type {
 
     @Override
-    public boolean isType(Class<?> clazz) {
+    public boolean isType(@NotNull Class<?> clazz) {
         return clazz.isPrimitive() || Utils.JAVA_ELEMENTS.contains(clazz);
     }
 }
