8828b4c3c3ebbc911c76a0990b7c3522a4391564
HttpMarco <mirco.lindenau@gmx.de>
2024-03-19 18:41:16 +0100

    Add h2 and sql layer

---
+++ evelon-sql-h2/src/main/java/dev/httpmarco/evelon/sql/h2/H2Layer.java
@@ -0,0 +1,6 @@
+package dev.httpmarco.evelon.sql.h2;
+
+import dev.httpmarco.evelon.sql.parent.layer.SqlParentLayer;
+
+public class H2Layer extends SqlParentLayer {
+}
---
+++ evelon-sql-parent/src/main/java/dev/httpmarco/evelon/sql/parent/layer/SqlParentLayer.java
@@ -0,0 +1,6 @@
+package dev.httpmarco.evelon.sql.parent.layer;
+
+import dev.httpmarco.evelon.layer.Layer;
+
+public abstract class SqlParentLayer implements Layer {
+}
