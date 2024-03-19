2a30839149d72852130f606194f222018ba2d8de
HttpMarco <mirco.lindenau@gmx.de>
2024-03-19 18:41:36 +0100

    Modify layer finding

--- evelon-common/src/main/java/dev/httpmarco/evelon/layer/LayerService.java
+++ evelon-common/src/main/java/dev/httpmarco/evelon/layer/LayerService.java
@@ -1,9 +1,8 @@
 package dev.httpmarco.evelon.layer;
 
-import dev.httpmarco.evelon.Evelon;
-import dev.httpmarco.osgan.reflections.ClassScanner;
 import dev.httpmarco.osgan.reflections.Reflections;
 import org.jetbrains.annotations.Nullable;
+
 import java.util.HashSet;
 import java.util.Set;
 
@@ -12,17 +11,16 @@ public final class LayerService {
     // main layer cache
     private final Set<Layer> layers = new HashSet<>();
 
-    public LayerService() {
-
-        for (var layerClass : ClassScanner.of(ClassLoader.getSystemClassLoader(), Evelon.class.getPackageName())
-                .criteria(aClass -> aClass.isAssignableFrom(Layer.class))
-                .scan()) {
-
-            var layer = (Layer) new Reflections<>(layerClass).allocate();
-
-            //todo initialize layer if they need a connection or something else
-            this.layers.add(layer);
-        }
+    /**
+     * Register a layer
+     *
+     * @param layerClass the layer class
+     * @return the layer
+     */
+    private Layer registerLayer(Class<? extends Layer> layerClass) {
+        var layer = new Reflections<>(layerClass).allocate();
+        this.layers.add(layer);
+        return layer;
     }
 
     /**
@@ -32,6 +30,6 @@ public final class LayerService {
      * @return the layer
      */
     public @Nullable Layer find(Class<? extends Layer> layer) {
-        return layers.stream().filter(it -> it.getClass().equals(layer)).findFirst().orElse(null);
+        return layers.stream().filter(it -> it.getClass().equals(layer)).findFirst().orElseGet(() -> this.registerLayer(layer));
     }
 }
