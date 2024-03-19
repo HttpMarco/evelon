0ca6c4adf6d060765f2bec8df0fe02cd43b05d14
HttpMarco <mirco.lindenau@gmx.de>
2024-03-19 22:32:52 +0100

    Implement initialize method

--- evelon-common/src/main/java/dev/httpmarco/evelon/layer/Layer.java
+++ evelon-common/src/main/java/dev/httpmarco/evelon/layer/Layer.java
@@ -1,7 +1,10 @@
 package dev.httpmarco.evelon.layer;
 
+import dev.httpmarco.evelon.process.common.InitializeProcess;
+import dev.httpmarco.evelon.repository.Repository;
+
 public interface Layer {
 
-
+    void initialize(Repository<?> repository, InitializeProcess process);
 
 }
--- evelon-sql-parent/src/main/java/dev/httpmarco/evelon/sql/parent/layer/SqlParentLayer.java
+++ evelon-sql-parent/src/main/java/dev/httpmarco/evelon/sql/parent/layer/SqlParentLayer.java
@@ -1,6 +1,13 @@
 package dev.httpmarco.evelon.sql.parent.layer;
 
 import dev.httpmarco.evelon.layer.Layer;
+import dev.httpmarco.evelon.process.common.InitializeProcess;
+import dev.httpmarco.evelon.repository.Repository;
 
 public abstract class SqlParentLayer implements Layer {
+
+    @Override
+    public void initialize(Repository<?> repository, InitializeProcess process) {
+        // todo create and load table
+    }
 }
