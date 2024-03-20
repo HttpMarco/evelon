8ebf6bb42586ed75bbd78c23209f1359bb80b1f6
HttxMarco <mircolindenau04@gmail.com>
2024-03-20 07:50:20 +0100

    Implement process and processmeta

--- evelon-common/src/main/java/dev/httpmarco/evelon/process/Process.java
+++ evelon-common/src/main/java/dev/httpmarco/evelon/process/Process.java
@@ -1,13 +1,24 @@
 package dev.httpmarco.evelon.process;
 
+import dev.httpmarco.evelon.repository.Repository;
 import dev.httpmarco.evelon.repository.RepositoryClass;
+import lombok.Getter;
+import lombok.experimental.Accessors;
 
 import java.util.LinkedHashSet;
 import java.util.Set;
 
+
+@Getter
+@Accessors(fluent = true)
 public abstract class Process implements Runnable {
 
     private String id;
+    private final ProcessMeta meta;
     private final Set<RepositoryClass<?>> affectedRows = new LinkedHashSet<>();
 
+    public Process(String id, Repository<?> repository) {
+        this.id = id;
+        this.meta = new ProcessMeta(repository);
+    }
 }
--- evelon-common/src/main/java/dev/httpmarco/evelon/process/ProcessMeta.java
+++ evelon-common/src/main/java/dev/httpmarco/evelon/process/ProcessMeta.java
@@ -1,4 +1,15 @@
 package dev.httpmarco.evelon.process;
 
+import dev.httpmarco.evelon.repository.Repository;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.experimental.Accessors;
+
+@Accessors(fluent = true)
+@Getter
+@AllArgsConstructor
 public class ProcessMeta {
+
+    private final Repository<?> repository;
+
 }
