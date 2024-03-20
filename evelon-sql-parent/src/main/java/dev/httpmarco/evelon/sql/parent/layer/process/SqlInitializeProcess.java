29c117e2274dc3d859d35795239e1031e433cfcd
HttxMarco <mircolindenau04@gmail.com>
2024-03-20 12:39:51 +0100

    Complete sql create table query

--- evelon-sql-parent/src/main/java/dev/httpmarco/evelon/sql/parent/layer/process/SqlInitializeProcess.java
+++ evelon-sql-parent/src/main/java/dev/httpmarco/evelon/sql/parent/layer/process/SqlInitializeProcess.java
@@ -2,22 +2,41 @@ package dev.httpmarco.evelon.sql.parent.layer.process;
 
 import dev.httpmarco.evelon.process.common.InitializeProcess;
 import dev.httpmarco.evelon.repository.Repository;
-import dev.httpmarco.evelon.repository.RepositoryClass;
 import dev.httpmarco.evelon.repository.RepositoryObjectClass;
+import dev.httpmarco.evelon.sql.parent.layer.SqlType;
+import dev.httpmarco.evelon.sql.parent.layer.connection.HikariConnectionTransmitter;
+
+import java.util.ArrayList;
 
 public class SqlInitializeProcess extends InitializeProcess {
 
     private static final String TABLE_CREATE_QUERY = "CREATE TABLE IF NOT EXISTS %s(%s);";
+    private final HikariConnectionTransmitter transmitter;
 
-    public SqlInitializeProcess(String id, Repository<?> repository) {
+    public SqlInitializeProcess(HikariConnectionTransmitter transmitter, String id, Repository<?> repository) {
         super(id, repository);
+        this.transmitter = transmitter;
     }
 
     @Override
     public void pushUpdate() {
-        var rows = affectedRows().stream().map(it -> ((RepositoryObjectClass.ObjectField<?>) it)).map(RepositoryObjectClass.ObjectField::id).toList();
 
-        String formattedQuery = TABLE_CREATE_QUERY.formatted(id(), String.join(", ", rows));
-        System.err.println(formattedQuery);
+        var queryTableParameter = new ArrayList<>(affectedRows()
+                .stream()
+                .map(it -> ((RepositoryObjectClass.ObjectField<?>) it))
+                .map(it -> it.id() + " " + SqlType.find(it.originalClass()))
+                .toList());
+
+        var primaryKeyList = affectedRows().stream()
+                .map(it -> ((RepositoryObjectClass.ObjectField<?>) it))
+                .filter(RepositoryObjectClass.ObjectField::primary)
+                .map(RepositoryObjectClass.ObjectField::id)
+                .toList();
+
+        if (!primaryKeyList.isEmpty()) {
+            queryTableParameter.add("PRIMARY KEY (%s)".formatted(String.join(", ", primaryKeyList)));
+        }
+
+        this.transmitter.executeUpdate(TABLE_CREATE_QUERY.formatted(id(), String.join(", ", queryTableParameter)), this);
     }
 }
