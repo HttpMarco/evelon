9a700bb92128ca94bfe9dd324cec7d0c2363bc72
HttxMarco <mircolindenau04@gmail.com>
2024-03-20 08:17:39 +0100

    Add response logic

---
+++ evelon-common/src/main/java/dev/httpmarco/evelon/query/response/QueryResponse.java
@@ -0,0 +1,16 @@
+package dev.httpmarco.evelon.query.response;
+
+import lombok.Getter;
+import lombok.experimental.Accessors;
+
+@Getter
+@Accessors(fluent = true)
+public final class QueryResponse<T> extends ResponseResult<QueryResponse<T>> {
+
+    private T result;
+
+    public QueryResponse<T> result(T result) {
+        this.result = result;
+        return this;
+    }
+}
---
+++ evelon-common/src/main/java/dev/httpmarco/evelon/query/response/UpdateResponse.java
@@ -0,0 +1,18 @@
+package dev.httpmarco.evelon.query.response;
+
+import lombok.Getter;
+import lombok.experimental.Accessors;
+
+@Getter
+@Accessors(fluent = true)
+public final class UpdateResponse extends ResponseResult<UpdateResponse> {
+
+    // only success information
+    private int modifiedElements = 0;
+
+    @Override
+    public void append(UpdateResponse response) {
+        super.append(response);
+        this.modifiedElements += response.modifiedElements;
+    }
+}
