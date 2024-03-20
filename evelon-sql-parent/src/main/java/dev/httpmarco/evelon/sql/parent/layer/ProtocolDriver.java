42e263922d55764c9845e84bf5038e017aec1307
HttxMarco <mircolindenau04@gmail.com>
2024-03-20 08:28:37 +0100

    Attach sql protocol driver

---
+++ evelon-sql-parent/src/main/java/dev/httpmarco/evelon/sql/parent/layer/ProtocolDriver.java
@@ -0,0 +1,15 @@
+package dev.httpmarco.evelon.sql.parent.layer;
+
+import dev.httpmarco.evelon.credentials.Credentials;
+
+public interface ProtocolDriver<D extends Credentials> {
+
+    void onInitialize();
+
+    String jdbcString(D credentials);
+
+    @SuppressWarnings("unchecked")
+    default String jdbcStringWithMapping(Credentials credentials) {
+        return this.jdbcString((D) credentials);
+    }
+}
