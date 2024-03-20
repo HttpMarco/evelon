e6e7fd8d064ec9d049fc39e6077a2ef51652e64b
HttxMarco <mircolindenau04@gmail.com>
2024-03-20 10:40:06 +0100

    Add layer on initialize header

--- evelon-common/src/main/java/dev/httpmarco/evelon/stage/SubStage.java
+++ evelon-common/src/main/java/dev/httpmarco/evelon/stage/SubStage.java
@@ -1,10 +1,11 @@
 package dev.httpmarco.evelon.stage;
 
+import dev.httpmarco.evelon.layer.Layer;
 import dev.httpmarco.evelon.process.common.InitializeProcess;
 import dev.httpmarco.evelon.repository.RepositoryObjectClass;
 
 public interface SubStage extends Stage {
 
-    void initialize(RepositoryObjectClass<?> clazz, InitializeProcess process);
+    void initialize(Layer layer, RepositoryObjectClass<?> clazz, InitializeProcess process);
 
 }
