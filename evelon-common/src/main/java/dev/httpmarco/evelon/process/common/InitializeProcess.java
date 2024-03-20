8e8f3c497e72e5b23cbaf8c84fab7abe73920649
HttxMarco <mircolindenau04@gmail.com>
2024-03-20 09:17:24 +0100

    Start working on initialize process

--- evelon-common/src/main/java/dev/httpmarco/evelon/process/common/InitializeProcess.java
+++ evelon-common/src/main/java/dev/httpmarco/evelon/process/common/InitializeProcess.java
@@ -1,11 +1,29 @@
 package dev.httpmarco.evelon.process.common;
 
+import dev.httpmarco.evelon.layer.Layer;
 import dev.httpmarco.evelon.process.Process;
+import dev.httpmarco.evelon.repository.Repository;
+import dev.httpmarco.evelon.repository.RepositoryObjectClass;
+import dev.httpmarco.evelon.stage.SubStage;
+import dev.httpmarco.evelon.stage.Type;
+import dev.httpmarco.osgan.utils.validate.Check;
 
 public class InitializeProcess extends Process {
 
+    public InitializeProcess(String id, Repository<?> repository) {
+        super(id, repository);
+    }
+
     @Override
     public void run() {
+        var repository = meta().repository();
+        var type = repository.clazz().type();
 
+        Check.stateCondition(type != Type.OBJECT, "The repository type is not an object");
+
+        for (Layer layer : repository.layers()) {
+            // initialize the layer
+            ((SubStage) layer.stage(type)).initialize((RepositoryObjectClass<?>) repository.clazz(), this);
+        }
     }
 }
--- evelon-common/src/main/java/dev/httpmarco/evelon/stage/common/ObjectSubStage.java
+++ evelon-common/src/main/java/dev/httpmarco/evelon/stage/common/ObjectSubStage.java
@@ -1,11 +1,25 @@
 package dev.httpmarco.evelon.stage.common;
 
+import dev.httpmarco.evelon.process.common.InitializeProcess;
+import dev.httpmarco.evelon.repository.RepositoryObjectClass;
 import dev.httpmarco.evelon.stage.SubStage;
 import dev.httpmarco.evelon.stage.Type;
 
 public final class ObjectSubStage implements SubStage {
+
     @Override
     public Type type() {
         return Type.OBJECT;
+    }
+
+    @Override
+    public void initialize(RepositoryObjectClass<?> clazz, InitializeProcess process) {
+        for (var field : clazz.fields()) {
+            if (field.type() == Type.OBJECT) {
+                // todo sub process
+            } else if (field.type() == Type.PARAMETER) {
+                process.affectedRows().add(field);
+            }
+        }
     }
 }
