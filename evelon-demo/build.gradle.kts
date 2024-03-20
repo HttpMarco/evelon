efd8c78e20404947e7d61dc4c913003e696ae82f
HttxMarco <mircolindenau04@gmail.com>
2024-03-20 08:33:16 +0100

    Add requirement connection to connectable layer

--- evelon-demo/build.gradle.kts
+++ evelon-demo/build.gradle.kts
@@ -1,10 +1,12 @@
 dependencies {
     implementation(project(":evelon-common"))
+    testRuntimeOnly(project(":evelon-common"))
 
     testImplementation(project(":evelon-sql-h2"))
     testImplementation(project(":evelon-sql-parent"))
     testImplementation(project(":evelon-common"))
 
+    testRuntimeOnly(libs.gson)
     testImplementation(platform("org.junit:junit-bom:5.10.2"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
