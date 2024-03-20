f7861dd292abd7a43d3e8fd09576ee8c0ffd6ac8
HttxMarco <mircolindenau04@gmail.com>
2024-03-20 08:33:45 +0100

    Setup h2 layer

--- evelon-sql-h2/src/main/java/dev/httpmarco/evelon/sql/h2/H2Layer.java
+++ evelon-sql-h2/src/main/java/dev/httpmarco/evelon/sql/h2/H2Layer.java
@@ -3,4 +3,8 @@ package dev.httpmarco.evelon.sql.h2;
 import dev.httpmarco.evelon.sql.parent.layer.SqlParentLayer;
 
 public class H2Layer extends SqlParentLayer {
+
+    public H2Layer() {
+        super(new H2Credentials());
+    }
 }
