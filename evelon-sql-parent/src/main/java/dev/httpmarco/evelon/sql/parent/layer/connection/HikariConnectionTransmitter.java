06ccf8d6c330e2966f3cf712f77c94adda248e04
HttxMarco <mircolindenau04@gmail.com>
2024-03-20 08:21:21 +0100

    Complete hikari connection transmitter

---
+++ evelon-sql-parent/src/main/java/dev/httpmarco/evelon/sql/parent/layer/connection/HikariConnectionTransmitter.java
@@ -0,0 +1,45 @@
+package dev.httpmarco.evelon.sql.parent.layer.connection;
+
+import dev.httpmarco.evelon.Evelon;
+import dev.httpmarco.evelon.process.Process;
+import dev.httpmarco.evelon.query.response.QueryResponse;
+import dev.httpmarco.evelon.query.response.ResponseResult;
+import dev.httpmarco.evelon.query.response.UpdateResponse;
+import lombok.AllArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Objects;
+
+@AllArgsConstructor
+public final class HikariConnectionTransmitter {
+
+    private HikariConnection hikariConnection;
+
+    public @NotNull UpdateResponse executeUpdate(final String query, Process process, Object... arguments) {
+        return this.transferPreparedStatement(it -> {
+            it.executeUpdate();
+            return new UpdateResponse();
+        }, query, process, arguments);
+    }
+
+
+    public <T> @NotNull QueryResponse<T> executeQuery(final String query, HikariConnectionFunction<ResultSet, T> function, Process process, Object... arguments) {
+        return this.transferPreparedStatement(it -> new QueryResponse<T>().result(function.apply(it.executeQuery())), query, process, arguments);
+    }
+
+    private <R extends ResponseResult<?>> @NotNull R transferPreparedStatement(HikariConnectionFunction<PreparedStatement, R> function, String query, Process process, @NotNull Object... arguments) {
+        Evelon.LOGGER.info("Executing query {}: {}", process, query);
+        try (var connection = hikariConnection.getConnection(); var statement = connection.prepareStatement(query)) {
+            for (int i = 0; i < arguments.length; i++) {
+                statement.setString(i + 1, Objects.toString(arguments[i]));
+            }
+            return function.apply(statement);
+        } catch (SQLException exception) {
+            throw new RuntimeException(exception);
+        }
+    }
+
+}
