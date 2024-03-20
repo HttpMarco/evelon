da823d787e0fa3bb15ad03741c1cb7375d055be0
HttxMarco <mircolindenau04@gmail.com>
2024-03-20 07:49:47 +0100

    Add initialize method for substages

--- evelon-common/src/main/java/dev/httpmarco/evelon/stage/SubStage.java
+++ evelon-common/src/main/java/dev/httpmarco/evelon/stage/SubStage.java
@@ -1,4 +1,10 @@
 package dev.httpmarco.evelon.stage;
 
+import dev.httpmarco.evelon.process.common.InitializeProcess;
+import dev.httpmarco.evelon.repository.RepositoryObjectClass;
+
 public interface SubStage extends Stage {
+
+    void initialize(RepositoryObjectClass<?> clazz, InitializeProcess process);
+
 }
