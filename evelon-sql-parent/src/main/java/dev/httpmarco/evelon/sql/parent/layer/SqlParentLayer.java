aee9a5fe491b0034d38113d054007c4e44601d73
HttxMarco <mircolindenau04@gmail.com>
2024-03-20 08:20:22 +0100

    Change sql layer to connectable layer

--- evelon-sql-parent/src/main/java/dev/httpmarco/evelon/sql/parent/layer/SqlParentLayer.java
+++ evelon-sql-parent/src/main/java/dev/httpmarco/evelon/sql/parent/layer/SqlParentLayer.java
@@ -1,13 +1,16 @@
 package dev.httpmarco.evelon.sql.parent.layer;
 
-import dev.httpmarco.evelon.layer.Layer;
-import dev.httpmarco.evelon.process.common.InitializeProcess;
-import dev.httpmarco.evelon.repository.Repository;
+import dev.httpmarco.evelon.credentials.Credentials;
+import dev.httpmarco.evelon.layer.ConnectableLayer;
+import dev.httpmarco.evelon.sql.parent.layer.credentials.AbstractSqlCredentials;
 
-public abstract class SqlParentLayer implements Layer {
+public abstract class SqlParentLayer extends ConnectableLayer {
 
-    @Override
-    public void initialize(Repository<?> repository, InitializeProcess process) {
-        // todo create and load table
+    public SqlParentLayer(Credentials templateCredentials) {
+        super(templateCredentials);
+    }
+
+    public SqlParentLayer(String id) {
+        super(new AbstractSqlCredentials(id, "127.0.0.1", "root", "secret"));
     }
 }
