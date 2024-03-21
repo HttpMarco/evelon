3091e3fb5fdf99847c44b89a96609c1e2fdcc91b
HttpMarco <mirco.lindenau@gmx.de>
2024-03-21 23:26:21 +0100

    Fix: Sql injection

--- evelon-sql-parent/src/main/java/dev/httpmarco/evelon/sql/parent/layer/process/SqlCreateProcess.java
+++ evelon-sql-parent/src/main/java/dev/httpmarco/evelon/sql/parent/layer/process/SqlCreateProcess.java
@@ -1,18 +1,18 @@
 package dev.httpmarco.evelon.sql.parent.layer.process;
 
 import dev.httpmarco.evelon.process.common.CreateProcess;
+import dev.httpmarco.evelon.query.Query;
 import dev.httpmarco.evelon.query.response.UpdateResponse;
-import dev.httpmarco.evelon.repository.Repository;
 import dev.httpmarco.evelon.repository.RepositoryObjectClass;
 import dev.httpmarco.evelon.sql.parent.layer.connection.HikariConnectionTransmitter;
 
-public class SqlCreateProcess extends CreateProcess {
+public class SqlCreateProcess<T> extends CreateProcess<T> {
 
     private static final String TABLE_CREATE_QUERY = "INSERT INTO %s(%s) VALUES (%s);";
     private final HikariConnectionTransmitter transmitter;
 
-    public SqlCreateProcess(HikariConnectionTransmitter transmitter, String id, Repository<?> repository) {
-        super(id, repository);
+    public SqlCreateProcess(HikariConnectionTransmitter transmitter, String id, Query<T> query) {
+        super(id, query);
         this.transmitter = transmitter;
     }
 
@@ -20,12 +20,11 @@ public class SqlCreateProcess extends CreateProcess {
     public UpdateResponse pushCreate(Object value) {
         UpdateResponse response = new UpdateResponse();
 
-        //todo with sql args
         response.append(transmitter.executeUpdate(TABLE_CREATE_QUERY.formatted(id(), String.join(", ", affectedRows().
                 stream()
                 .map(it -> ((RepositoryObjectClass.ObjectField<?>) it).id())
                 .toList()), String.join(", ",
-                affectedRows().stream().map(it -> ((RepositoryObjectClass.ObjectField<?>) it).fieldValue(value).toString()).toList())), this));
+                affectedRows().stream().map(it -> "?").toList())), this, affectedRows().stream().map(it -> ((RepositoryObjectClass.ObjectField<?>) it).fieldValue(value)).toArray()));
 
         return response;
     }
