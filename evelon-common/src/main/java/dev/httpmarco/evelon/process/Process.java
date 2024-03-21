8e746236795c19b9bf8a3b6e2d45e88d8d9bd381
HttxMarco <mircolindenau04@gmail.com>
2024-03-21 08:10:42 +0100

    Clean process code

--- evelon-common/src/main/java/dev/httpmarco/evelon/process/Process.java
+++ evelon-common/src/main/java/dev/httpmarco/evelon/process/Process.java
@@ -1,28 +1,43 @@
 package dev.httpmarco.evelon.process;
 
+import dev.httpmarco.evelon.layer.Layer;
 import dev.httpmarco.evelon.repository.Repository;
 import dev.httpmarco.evelon.repository.RepositoryClass;
+import dev.httpmarco.evelon.repository.RepositoryObjectClass;
+import dev.httpmarco.evelon.stage.SubStage;
+import dev.httpmarco.evelon.stage.Type;
+import dev.httpmarco.osgan.utils.validate.Check;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.Accessors;
 
 import java.util.LinkedHashSet;
-import java.util.List;
 import java.util.Set;
 
 
 @Getter
 @Accessors(fluent = true)
-public abstract class Process<P extends Process<?>> implements Runnable {
+public abstract class Process {
 
     @Setter
     private String id;
     private final ProcessMeta meta;
-
     private final Set<RepositoryClass<?>> affectedRows = new LinkedHashSet<>();
 
     public Process(String id, Repository<?> repository) {
         this.id = id;
         this.meta = new ProcessMeta(repository);
+
+        this.prepare();
+    }
+
+    public void prepare() {
+        var type = meta().repository().clazz().type();
+
+        Check.stateCondition(type != Type.OBJECT, "The repository type is not an object type: " + type);
+
+        for (var layer : meta().repository().layers()) {
+            ((SubStage) layer.stage(type)).attachAffectedRows(this, (RepositoryObjectClass<?>) meta().repository().clazz());
+        }
     }
 }
