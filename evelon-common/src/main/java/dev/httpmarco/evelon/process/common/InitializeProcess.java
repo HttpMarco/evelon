21aa8d6887f3cd03b420b248da3022dd1f914f33
HttxMarco <mircolindenau04@gmail.com>
2024-03-21 08:11:12 +0100

    Remove duplicated run method

--- evelon-common/src/main/java/dev/httpmarco/evelon/process/common/InitializeProcess.java
+++ evelon-common/src/main/java/dev/httpmarco/evelon/process/common/InitializeProcess.java
@@ -1,30 +1,12 @@
 package dev.httpmarco.evelon.process.common;
 
-import dev.httpmarco.evelon.layer.Layer;
 import dev.httpmarco.evelon.process.Process;
 import dev.httpmarco.evelon.repository.Repository;
-import dev.httpmarco.evelon.repository.RepositoryObjectClass;
-import dev.httpmarco.evelon.stage.SubStage;
-import dev.httpmarco.evelon.stage.Type;
-import dev.httpmarco.osgan.utils.validate.Check;
 
-public abstract class InitializeProcess extends Process<InitializeProcess> {
+public abstract class InitializeProcess extends Process {
 
     public InitializeProcess(String id, Repository<?> repository) {
         super(id, repository);
-    }
-
-    @Override
-    public void run() {
-        var repository = meta().repository();
-        var type = repository.clazz().type();
-
-        Check.stateCondition(type != Type.OBJECT, "The repository type is not an object type: " + type);
-
-        for (Layer layer : repository.layers()) {
-            // initialize the layer
-            ((SubStage) layer.stage(type)).initialize(layer, (RepositoryObjectClass<?>) repository.clazz(), this);
-        }
     }
 
     public abstract void pushInitialize();
--- evelon-common/src/main/java/dev/httpmarco/evelon/stage/SubStage.java
+++ evelon-common/src/main/java/dev/httpmarco/evelon/stage/SubStage.java
@@ -1,11 +1,9 @@
 package dev.httpmarco.evelon.stage;
 
-import dev.httpmarco.evelon.layer.Layer;
-import dev.httpmarco.evelon.process.common.InitializeProcess;
+import dev.httpmarco.evelon.process.Process;
 import dev.httpmarco.evelon.repository.RepositoryObjectClass;
 
 public interface SubStage extends Stage {
 
-    void initialize(Layer layer, RepositoryObjectClass<?> clazz, InitializeProcess process);
-
+    void attachAffectedRows(Process process, RepositoryObjectClass<?> clazz);
 }
