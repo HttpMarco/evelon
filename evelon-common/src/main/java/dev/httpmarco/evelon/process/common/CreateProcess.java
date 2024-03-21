682ba2be14bbf0926548d34f0c721ba33d8c64fb
HttxMarco <mircolindenau04@gmail.com>
2024-03-21 08:08:46 +0100

    Remove run method

--- evelon-common/src/main/java/dev/httpmarco/evelon/process/common/CreateProcess.java
+++ evelon-common/src/main/java/dev/httpmarco/evelon/process/common/CreateProcess.java
@@ -4,16 +4,10 @@ import dev.httpmarco.evelon.process.Process;
 import dev.httpmarco.evelon.query.response.UpdateResponse;
 import dev.httpmarco.evelon.repository.Repository;
 
-public abstract class CreateProcess extends Process<CreateProcess> {
+public abstract class CreateProcess extends Process {
 
     public CreateProcess(String id, Repository<?> repository) {
         super(id, repository);
-    }
-
-
-    @Override
-    public void run() {
-
     }
 
     public abstract UpdateResponse pushCreate(Object value);
