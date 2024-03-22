fc04f5fecdcd6966a0f610174d0a4e42c75deb1d
HttpMarco <mirco.lindenau@gmx.de>
2024-03-22 18:23:53 +0100

    Fix useless logging

--- evelon-sql-parent/src/main/java/dev/httpmarco/evelon/sql/parent/layer/connection/HikariConnectionTransmitter.java
+++ evelon-sql-parent/src/main/java/dev/httpmarco/evelon/sql/parent/layer/connection/HikariConnectionTransmitter.java
@@ -27,20 +27,20 @@ public final class HikariConnectionTransmitter {
     }
 
 
-    public <T> @NotNull QueryResponse<T> executeQuery(final String query, HikariConnectionFunction<ResultSet, T> function, Process process, Object... arguments) {
+    public <T> @NotNull QueryResponse<T> executeQuery(final String query, HikariConnectionFunction<ResultSet, T> function, Process<?> process, Object... arguments) {
         return this.transferPreparedStatement(it -> new QueryResponse<T>().result(function.apply(it.executeQuery())), query, process, arguments);
     }
 
-    private <R extends ResponseResult<?>> @NotNull R transferPreparedStatement(HikariConnectionFunction<PreparedStatement, R> function, String query, Process process, @NotNull Object... arguments) {
-        Evelon.LOGGER.info("Executing query {}: {}", process.getClass().getSimpleName(), query);
+    private <R extends ResponseResult<?>> @NotNull R transferPreparedStatement(HikariConnectionFunction<PreparedStatement, R> function, String query, Process<?> process, @NotNull Object... arguments) {
+        Evelon.LOGGER.debug("Executing query {}: {}", process.getClass().getSimpleName(), query);
         try (var connection = hikariConnection.getConnection(); var statement = connection.prepareStatement(query)) {
             for (int i = 0; i < arguments.length; i++) {
                 statement.setString(i + 1, Objects.toString(arguments[i]));
             }
             return function.apply(statement);
         } catch (SQLException exception) {
+            Evelon.LOGGER.error("Executing query {}: {}", process.getClass().getSimpleName(), query);
             throw new RuntimeException(exception);
         }
     }
-
 }
