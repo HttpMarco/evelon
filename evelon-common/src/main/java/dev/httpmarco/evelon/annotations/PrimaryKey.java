2b97f4a37287de479112d0d310620e05c257b69b
HttxMarco <mircolindenau04@gmail.com>
2024-04-09 18:28:59 +0200

    Add commentaries

--- evelon-common/src/main/java/dev/httpmarco/evelon/annotations/PrimaryKey.java
+++ evelon-common/src/main/java/dev/httpmarco/evelon/annotations/PrimaryKey.java
@@ -5,6 +5,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * PrimaryKey presents fields, witch are primary keys in database.
+ * It must not be null and must be unique.
+ */
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface PrimaryKey {
--- evelon-common/src/main/java/dev/httpmarco/evelon/annotations/Row.java
+++ evelon-common/src/main/java/dev/httpmarco/evelon/annotations/Row.java
@@ -5,12 +5,26 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Row annotation
+ * This annotation is used to mark a field with options
+ * Only a setting is required
+ */
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Row {
 
+    /**
+     * Overwrite the field name with the given value
+     * So you can overwrite the field name with the column name
+     * @return the name
+     */
     String id() default "";
 
+    /**
+     * Ignore the field
+     * @return true if the field should be ignored
+     */
     boolean ignore() default false;
 
 }
