eb0ae02e21f7f73333c060d907cf84e438f9fe8b
HttxMarco <mircolindenau04@gmail.com>
2024-04-09 18:56:09 +0200

    Add dependencies

--- evelon-demo/build.gradle.kts
+++ evelon-demo/build.gradle.kts
@@ -2,6 +2,10 @@ dependencies {
     testImplementation(libs.gson)
     testRuntimeOnly(project(":evelon-common"))
     testImplementation(project(":evelon-common"))
+
+    testImplementation(project(":evelon-sql-parent"))
+    testImplementation(project(":evelon-sql-h2"))
+
     testImplementation(platform("org.junit:junit-bom:5.10.2"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
