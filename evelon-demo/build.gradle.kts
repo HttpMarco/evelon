2e57d8ae071b75e0bc1112dad43fbd4fd66d5afd
HttxMarco <mircolindenau04@gmail.com>
2024-04-09 12:20:44 +0200

    Small gradle changes

---
+++ evelon-demo/build.gradle.kts
@@ -0,0 +1,14 @@
+dependencies {
+    testRuntimeOnly(project(":evelon-common"))
+    testImplementation(project(":evelon-common"))
+    testImplementation(platform("org.junit:junit-bom:5.10.2"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.test {
+    useJUnitPlatform()
+    testLogging {
+        events("passed", "skipped", "failed")
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,4 @@
 rootProject.name = "evelon"
 
 include("evelon-common")
+include("evelon-demo")
