df35e9107b9a36fa1743efaad331a757cca53a3b
HttxMarco <mircolindenau04@gmail.com>
2024-04-09 14:40:21 +0200

    Complete small layer service

---
+++ evelon-common/src/main/java/dev/httpmarco/evelon/layer/LayerService.java
@@ -0,0 +1,25 @@
+package dev.httpmarco.evelon.layer;
+
+import dev.httpmarco.evelon.layer.connection.ConnectableLayer;
+import dev.httpmarco.evelon.layer.connection.credentials.LayerConnectionCredentials;
+import lombok.SneakyThrows;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public final class LayerService {
+
+    private static final Map<Class<? extends Layer>, Layer> layers = new HashMap<>();
+
+    public static Layer layerOf(Class<? extends Layer> layer) {
+        return layers.computeIfAbsent(layer, LayerService::createLayer);
+    }
+
+    @Contract(pure = true)
+    @SneakyThrows
+    private static <T extends Layer> @NotNull T createLayer(Class<T> layer) {
+        return layer.getConstructor().newInstance();
+    }
+}
