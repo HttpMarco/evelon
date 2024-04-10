de7041d283def517fc846c44e542e43d7d729914
HttxMarco <mircolindenau04@gmail.com>
2024-04-10 09:02:14 +0200

    Implement first sql connection

---
+++ evelon-sql-parent/src/main/java/dev/httpmarco/evelon/sql/parent/HikariConnection.java
@@ -0,0 +1,54 @@
+package dev.httpmarco.evelon.sql.parent;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import dev.httpmarco.evelon.layer.connection.Connection;
+import dev.httpmarco.evelon.layer.connection.credentials.LayerConnectionCredentials;
+import org.jetbrains.annotations.Nullable;
+
+public final class HikariConnection implements Connection<HikariDataSource> {
+
+    private @Nullable HikariDataSource dataSource;
+
+    @Override
+    public void connect(LayerConnectionCredentials credentials) {
+        var config = new HikariConfig();
+
+        config.addDataSourceProperty("cachePrepStmts", "true");
+        config.addDataSourceProperty("prepStmtCacheSize", "250");
+        config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+        config.addDataSourceProperty("useServerPrepStmts", "true");
+        config.addDataSourceProperty("useLocalSessionState", "true");
+        config.addDataSourceProperty("rewriteBatchedStatements", "true");
+        config.addDataSourceProperty("cacheResultSetMetadata", "true");
+        config.addDataSourceProperty("cacheServerConfiguration", "true");
+        config.addDataSourceProperty("elideSetAutoCommits", "true");
+        config.addDataSourceProperty("maintainTimeStats", "false");
+
+        config.setMinimumIdle(2);
+        config.setMaximumPoolSize(100);
+        config.setConnectionTimeout(10_000);
+        config.setValidationTimeout(10_000);
+
+        // todo
+        // config.setJdbcUrl("jdbc:" + protocolDriver.jdbcStringWithMapping(credentials));
+        this.dataSource = new HikariDataSource(config);
+    }
+
+    @Override
+    public boolean isConnected() {
+        return this.dataSource != null && !this.dataSource.isClosed();
+    }
+
+    @Override
+    public void close() {
+        if (this.dataSource != null && !this.dataSource.isClosed()) {
+            this.dataSource.close();
+        }
+    }
+
+    @Override
+    public HikariDataSource connection() {
+        return this.dataSource;
+    }
+}
