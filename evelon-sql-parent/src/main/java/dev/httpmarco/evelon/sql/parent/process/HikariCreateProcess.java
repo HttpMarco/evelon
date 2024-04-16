53406add9993ad9b176fcf4e692ae7b94fac50df
HttxMarco <mircolindenau04@gmail.com>
2024-04-16 10:13:07 +0200

    Finished simple create statement

--- evelon-sql-parent/src/main/java/dev/httpmarco/evelon/sql/parent/process/HikariCreateProcess.java
+++ evelon-sql-parent/src/main/java/dev/httpmarco/evelon/sql/parent/process/HikariCreateProcess.java
@@ -3,7 +3,10 @@ package dev.httpmarco.evelon.sql.parent.process;
 import dev.httpmarco.evelon.layer.Layer;
 import dev.httpmarco.evelon.process.Process;
 import dev.httpmarco.evelon.repository.RepositoryEntry;
+import dev.httpmarco.evelon.repository.RepositoryExternalEntry;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
 
 public final class HikariCreateProcess extends Process<String, HikariCreateProcess> {
 
@@ -11,9 +14,21 @@ public final class HikariCreateProcess extends Process<String, HikariCreateProce
 
     @Override
     public String run(@NotNull RepositoryEntry entry, Layer<String> layer) {
+        if (!(entry instanceof RepositoryExternalEntry objectEntry)) {
+            throw new UnsupportedOperationException("Processes can only be run with external entries!");
+        }
 
+        var sqlEntries = new ArrayList<String>();
+        for (var child : objectEntry.children()) {
+            if (child instanceof RepositoryExternalEntry) {
+                this.newSubProcess(new HikariCreateProcess());
+                continue;
+            }
+            sqlEntries.add(child.id());
+        }
 
-
-        return CREATE_VALUE_SQL.formatted(entry.id(), "", "");
+        return CREATE_VALUE_SQL.formatted(entry.id(),
+                String.join(", ", sqlEntries),
+                String.join(", ", "?".repeat(sqlEntries.size()).split("")));
     }
 }
