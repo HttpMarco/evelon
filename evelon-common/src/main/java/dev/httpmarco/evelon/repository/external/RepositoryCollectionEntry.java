801381164db3e6259a8ac4ab9dbe8b07be05a0b3
HttxMarco <mircolindenau04@gmail.com>
2024-04-18 10:16:13 +0200

    Add foreign reference for collections

--- evelon-common/src/main/java/dev/httpmarco/evelon/repository/external/RepositoryCollectionEntry.java
+++ evelon-common/src/main/java/dev/httpmarco/evelon/repository/external/RepositoryCollectionEntry.java
@@ -1,13 +1,18 @@
 package dev.httpmarco.evelon.repository.external;
 
 import dev.httpmarco.evelon.Evelon;
+import dev.httpmarco.evelon.repository.RepositoryConstant;
 import dev.httpmarco.evelon.repository.RepositoryEntry;
 import dev.httpmarco.evelon.repository.RepositoryEntryType;
 import dev.httpmarco.evelon.repository.RepositoryExternalEntry;
+import lombok.Getter;
+import lombok.experimental.Accessors;
 import org.jetbrains.annotations.NotNull;
 
 import java.lang.reflect.Field;
 
+@Getter
+@Accessors(fluent = true)
 public final class RepositoryCollectionEntry extends RepositoryExternalEntry {
 
     private final RepositoryEntry componentEntry;
@@ -16,5 +21,6 @@ public final class RepositoryCollectionEntry extends RepositoryExternalEntry {
         super(id, field.getType(), RepositoryEntryType.COLLECTION);
 
         this.componentEntry = RepositoryEntryType.find(field.getName() + "_value", Evelon.generics(field)[0]);
+        this.componentEntry.constants().put(RepositoryConstant.FOREIGN_REFERENCE, this);
     }
 }
