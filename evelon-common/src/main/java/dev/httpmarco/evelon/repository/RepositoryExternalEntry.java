23ea4cd6b52b6bbbba32fb9cba1eafefb343e92b
HttxMarco <mircolindenau04@gmail.com>
2024-04-18 13:09:48 +0200

    Add method for get all primaries

--- evelon-common/src/main/java/dev/httpmarco/evelon/repository/RepositoryExternalEntry.java
+++ evelon-common/src/main/java/dev/httpmarco/evelon/repository/RepositoryExternalEntry.java
@@ -3,17 +3,20 @@ package dev.httpmarco.evelon.repository;
 import lombok.Getter;
 import lombok.experimental.Accessors;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 @Getter
 @Accessors(fluent = true)
 public abstract class RepositoryExternalEntry extends RepositoryEntry  {
 
-    private final List<RepositoryEntry> children = new ArrayList<>();
+    private final List<RepositoryEntry> children = new LinkedList<>();
 
     public RepositoryExternalEntry(String id, Class<?> clazz, RepositoryEntryType type) {
         super(id, clazz, type);
     }
 
+    public List<RepositoryEntry> primaries() {
+        return this.children.stream().filter(it -> it.constants().has(RepositoryConstant.PRIMARY_KEY)).toList();
+    }
 }
