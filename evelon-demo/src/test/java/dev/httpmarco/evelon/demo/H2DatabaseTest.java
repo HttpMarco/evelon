ca94a3bd76a774390c562decee0ccc80d19805c0
HttpMarco <mirco.lindenau@gmx.de>
2024-04-19 18:06:07 +0200

    Add missing creation tests

--- evelon-demo/src/test/java/dev/httpmarco/evelon/demo/H2DatabaseTest.java
+++ evelon-demo/src/test/java/dev/httpmarco/evelon/demo/H2DatabaseTest.java
@@ -3,10 +3,15 @@ package dev.httpmarco.evelon.demo;
 import dev.httpmarco.evelon.demo.models.ComplexListModel;
 import dev.httpmarco.evelon.demo.models.SimpleListModel;
 import dev.httpmarco.evelon.demo.models.SimpleModel;
+import dev.httpmarco.evelon.demo.models.objects.TestObject1;
+import dev.httpmarco.evelon.demo.models.objects.TestObject2;
 import dev.httpmarco.evelon.repository.Repository;
 import dev.httpmarco.evelon.sql.h2.H2Layer;
 import org.jetbrains.annotations.Async;
 import org.junit.jupiter.api.*;
+
+import java.util.List;
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -54,6 +59,13 @@ public final class H2DatabaseTest {
         }
 
         @Test
+        @Order(1)
+        void create() {
+            REPOSITORY.query().create(new SimpleListModel(UUID.randomUUID(), List.of("a", "b"), List.of("admin", "test")));
+        }
+
+
+        @Test
         @Order(20)
         void delete() {
             REPOSITORY.query().delete();
@@ -71,6 +83,12 @@ public final class H2DatabaseTest {
         @Order(0)
         void initialize() {
             assertNotNull(REPOSITORY = Repository.build(ComplexListModel.class).withId("persons_complex").withLayer(H2Layer.class).build());
+        }
+
+        @Test
+        @Order(1)
+        void create() {
+            REPOSITORY.query().create(new ComplexListModel(UUID.randomUUID(), List.of(new TestObject1("abc", 2000), new TestObject1("abc", 2000)), List.of(new TestObject2("admin", -1))));
         }
 
 
