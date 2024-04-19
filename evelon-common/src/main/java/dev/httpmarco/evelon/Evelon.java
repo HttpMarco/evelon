ade2d798fe9f49f79c0116ede3846795e848bf62
HttpMarco <mirco.lindenau@gmx.de>
2024-04-19 18:06:38 +0200

    Replace generic method with osgan

--- evelon-common/src/main/java/dev/httpmarco/evelon/Evelon.java
+++ evelon-common/src/main/java/dev/httpmarco/evelon/Evelon.java
@@ -3,22 +3,9 @@ package dev.httpmarco.evelon;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.ParameterizedType;
-import java.util.Arrays;
-
 public final class Evelon {
 
     // main logger of evelon
     public static final Logger LOGGER = LoggerFactory.getLogger(Evelon.class);
 
-    // todo replace with osgan-reflections
-    public static Class<?>[] generics(Field field) {
-        var genericType = field.getGenericType();
-        if (genericType instanceof ParameterizedType parameterizedType) {
-            return Arrays.stream(parameterizedType.getActualTypeArguments()).map(type -> (Class<?>) type).toArray(value -> new Class<?>[value]);
-        } else {
-            throw new UnsupportedOperationException("Cannot read generic from field: " + field.getName());
-        }
-    }
 }
