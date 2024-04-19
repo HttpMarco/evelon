5faf93337fdb934da8c89c173da7b7fcd6ec2f17
HttpMarco <mirco.lindenau@gmx.de>
2024-04-19 23:51:21 +0200

    Improve prepped process

--- evelon-sql-parent/src/main/java/dev/httpmarco/evelon/sql/parent/process/HikariPreppedProcess.java
+++ evelon-sql-parent/src/main/java/dev/httpmarco/evelon/sql/parent/process/HikariPreppedProcess.java
@@ -2,7 +2,6 @@ package dev.httpmarco.evelon.sql.parent.process;
 
 import dev.httpmarco.evelon.process.AbstractEntryProcess;
 import dev.httpmarco.evelon.repository.RepositoryConstant;
-import dev.httpmarco.evelon.repository.RepositoryEntry;
 import dev.httpmarco.evelon.repository.RepositoryExternalEntry;
 import dev.httpmarco.evelon.sql.parent.HikariExecutionReference;
 import dev.httpmarco.evelon.sql.parent.HikariRepositoryConstant;
@@ -13,15 +12,15 @@ import java.util.ArrayList;
 
 public final class HikariPreppedProcess extends AbstractEntryProcess<HikariExecutionReference> {
 
-    private static final String CREATE_TABLE_SQL = "CREATE TABLE IF NOT EXISTS %s (%s);";
+    private static final String TABLE_CREATE_QUERY = "CREATE TABLE IF NOT EXISTS %s (%s);";
+    private static final String TABLE_VALUE_FORMAT = "%s %s";
     private static final String FOREIGN_FORMAT = "FOREIGN KEY (%s) REFERENCES %s(%s)";
     private static final String PRIMARY_FORMAT = "PRIMARY KEY (%s)";
 
     @Override
     public @NotNull HikariExecutionReference run(@NotNull RepositoryExternalEntry entry) {
-
-        var sqlEntries = new ArrayList<String>();
-        var sqlPrimaryKeys = new ArrayList<String>();
+        var elements = new ArrayList<String>();
+        var primaries = new ArrayList<String>();
         var reference = new HikariExecutionReference();
 
         for (var child : entry.children()) {
@@ -32,27 +31,26 @@ public final class HikariPreppedProcess extends AbstractEntryProcess<HikariExecu
             }
 
             if (child.constants().has(RepositoryConstant.PRIMARY_KEY)) {
-                sqlPrimaryKeys.add(child.id());
+                primaries.add(child.id());
             }
 
-            var type = SqlType.find(child);
             // on first initialization, we put the sql type into the constants
-            child.constants().put(HikariRepositoryConstant.SQL_TYPE, type);
-            sqlEntries.add(child.id() + " " + type);
+            var type = child.constants().put(HikariRepositoryConstant.SQL_TYPE, SqlType.detect(child));
+            elements.add(TABLE_VALUE_FORMAT.formatted(child.id(), type));
         }
 
-        if (!sqlPrimaryKeys.isEmpty()) {
-            sqlEntries.add(PRIMARY_FORMAT.formatted(String.join(", ", sqlPrimaryKeys)));
+        if (!primaries.isEmpty()) {
+            elements.add(PRIMARY_FORMAT.formatted(String.join(", ", primaries)));
         }
 
         if (entry.constants().has(RepositoryConstant.FOREIGN_REFERENCE)) {
             for (var foreignKey : entry.constants().get(RepositoryConstant.FOREIGN_REFERENCE)) {
-                sqlEntries.add(foreignKey.id() + " " + foreignKey.constants().get(HikariRepositoryConstant.SQL_TYPE));
-                sqlEntries.add(FOREIGN_FORMAT.formatted(foreignKey.id(), foreignKey.parent().id(), foreignKey.id()));
+                // for a better format we put all primaries oder foreign keys in front of the sql entries
+                elements.add(0, TABLE_VALUE_FORMAT.formatted(foreignKey.id(), foreignKey.constants().get(HikariRepositoryConstant.SQL_TYPE)));
+                // last we add the foreign constraint
+                elements.add(FOREIGN_FORMAT.formatted(foreignKey.id(), foreignKey.parent().id(), foreignKey.id()));
             }
         }
-
-        reference.stack(CREATE_TABLE_SQL.formatted(entry.id(), String.join(", ", sqlEntries)));
-        return reference;
+        return reference.bind(TABLE_CREATE_QUERY.formatted(entry.id(), String.join(", ", elements)));
     }
 }
