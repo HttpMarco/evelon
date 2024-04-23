723ccb70cf33c1d0dc57be71313b36698600b253
HttxMarco <mircolindenau04@gmail.com>
2024-04-23 10:12:14 +0200

    Better method handling for properties

--- evelon-common/src/main/java/dev/httpmarco/evelon/process/Process.java
+++ evelon-common/src/main/java/dev/httpmarco/evelon/process/Process.java
@@ -1,16 +1,18 @@
 package dev.httpmarco.evelon.process;
 
-import lombok.Getter;
-import lombok.experimental.Accessors;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
-@Getter
-@Accessors(fluent = true)
 public abstract class Process {
 
     // for example for put the foreign value in the next sub process
-    private final Map<String, Object> properties = Map.of();
+    private final Map<Object, Object> properties = new LinkedHashMap<>();
 
+    public void property(Object key, Object value) {
+        properties.put(key.toString(), value);
+    }
+
+    public Object property(Object key) {
+        return properties.get(key.toString());
+    }
 }
