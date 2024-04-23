677986e36118d3ad28a641cd478cfc6177cd9df1
HttxMarco <mircolindenau04@gmail.com>
2024-04-23 09:47:14 +0200

    Remove arguments and add properties

--- evelon-common/src/main/java/dev/httpmarco/evelon/process/Process.java
+++ evelon-common/src/main/java/dev/httpmarco/evelon/process/Process.java
@@ -4,11 +4,13 @@ import lombok.Getter;
 import lombok.experimental.Accessors;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @Getter
 @Accessors(fluent = true)
 public abstract class Process {
 
-    private final List<Object> arguments = new ArrayList<>();
+    // for example for put the foreign value in the next sub process
+    private final Map<String, Object> properties = Map.of();
 
 }
