7b5af28924bc5f063aec2acd00d10fd79b38d378
HttxMarco <mircolindenau04@gmail.com>
2024-04-23 13:49:49 +0200

    Remove serializer

--- evelon-common/src/main/java/dev/httpmarco/evelon/EntrySerializer.java
+++
@@ -1,9 +0,0 @@
-package dev.httpmarco.evelon;
-
-public interface EntrySerializer<T, C, E extends RepositoryEntry> {
-
-    C serialize(T value, E entry);
-
-    T deserialize(C content, E entry);
-
-}
