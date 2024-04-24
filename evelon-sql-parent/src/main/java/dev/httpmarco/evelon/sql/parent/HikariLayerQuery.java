0ce5bff3840fd4df97f9c8306e90449545ef4b46
HttxMarco <mircolindenau04@gmail.com>
2024-04-24 11:29:18 +0200

    Small runner changes

--- evelon-sql-parent/src/main/java/dev/httpmarco/evelon/sql/parent/HikariLayerQuery.java
+++ evelon-sql-parent/src/main/java/dev/httpmarco/evelon/sql/parent/HikariLayerQuery.java
@@ -5,7 +5,8 @@ import dev.httpmarco.evelon.process.ProcessRunner;
 import dev.httpmarco.evelon.Repository;
 import dev.httpmarco.evelon.sql.parent.process.HikariCreateProcessAbstract;
 import dev.httpmarco.evelon.sql.parent.process.HikariDeleteProcess;
-import dev.httpmarco.evelon.sql.parent.reference.HikariExecutionProcessReference;
+import dev.httpmarco.evelon.sql.parent.process.HikariFindProcess;
+import dev.httpmarco.evelon.sql.parent.reference.HikariProcessReference;
 import lombok.AllArgsConstructor;
 
 import java.util.List;
@@ -14,20 +15,21 @@ import java.util.List;
 public final class HikariLayerQuery<T> implements LayerQuery<T> {
 
     private final Repository<T> repository;
-    private ProcessRunner<HikariExecutionProcessReference> runner;
+    private ProcessRunner<HikariProcessReference> runner;
 
     @Override
     public void create(T value) {
-        runner.update(new HikariCreateProcessAbstract(value), repository);
+        runner.apply(new HikariCreateProcessAbstract(value), repository);
     }
 
     @Override
     public void delete() {
-        runner.update(new HikariDeleteProcess(), repository);
+        runner.apply(new HikariDeleteProcess(), repository);
     }
 
     @Override
+    @SuppressWarnings("unchecked")
     public List<T> find() {
-        return List.of();
+        return (List<T>) runner.apply(new HikariFindProcess(),  repository);
     }
 }
