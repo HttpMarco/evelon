ac9cf09d0896b9362d208d0fe7eec3ed5780aa88
HttxMarco <mircolindenau04@gmail.com>
2024-04-24 11:09:45 +0200

    Add query and update separate process

---
+++ evelon-common/src/main/java/dev/httpmarco/evelon/process/kind/UpdateProcess.java
@@ -0,0 +1,15 @@
+package dev.httpmarco.evelon.process.kind;
+
+import dev.httpmarco.evelon.RepositoryExternalEntry;
+import dev.httpmarco.evelon.process.Process;
+import dev.httpmarco.evelon.process.ProcessReference;
+
+public abstract class UpdateProcess<R extends ProcessReference<R>> extends Process {
+
+     public abstract void run(RepositoryExternalEntry entry, R reference);
+
+     public void runMapping(RepositoryExternalEntry entry, ProcessReference<?> reference) {
+            run(entry, (R) reference);
+     }
+
+}
