a2ff02245f25856ac3ba383f73382cde566c5d1b
HttpMarco <mirco.lindenau@gmx.de>
2024-04-28 19:33:26 +0200

    Implement nexus publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,13 @@
+plugins {
+    alias(libs.plugins.nexus.publish)
+}
+
+
 allprojects {
     apply(plugin = "java-library")
+    apply(plugin = "maven-publish")
 
-    version = "1.0.0.0-beta"
+    version = "1.0.0-SNAPSHOT"
     group = "dev.httpmarco.evelon"
 
     repositories {
@@ -19,4 +25,48 @@ allprojects {
         targetCompatibility = JavaVersion.VERSION_17.toString()
         options.encoding = "UTF-8"
     }
+
+    extensions.configure<PublishingExtension> {
+        publications {
+            create("library", MavenPublication::class.java) {
+                from(project.components.getByName("java"))
+
+                pom {
+                    name.set(project.name)
+                    url.set("https://github.com/httpmarco/evelon")
+                    description.set("Reflection/Data libary")
+                    licenses {
+                        license {
+                            name.set("Apache License")
+                            url.set("https://www.apache.org/licenses/LICENSE-2.0")
+                        }
+                    }
+                    developers {
+                        developer {
+                            name.set("Mirco Lindenau")
+                            email.set("mirco.lindenau@gmx.de")
+                        }
+                    }
+
+                    scm {
+                        url.set("https://github.com/httpmarco/evelon")
+                        connection.set("https://github.com/httpmarco/evelon.git")
+                    }
+                }
+            }
+        }
+    }
+}
+
+nexusPublishing {
+    repositories {
+        sonatype {
+            nexusUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/releases/"))
+            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+
+            username.set(System.getenv("ossrhUsername")?.toString() ?: "")
+            password.set(System.getenv("ossrhPassword")?.toString() ?: "")
+        }
+    }
+    useStaging.set(!project.rootProject.version.toString().endsWith("-SNAPSHOT"))
 }
