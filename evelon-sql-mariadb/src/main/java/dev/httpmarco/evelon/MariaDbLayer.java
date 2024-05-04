6e8cadf393ca14ca0ac273b876f61ce5b7ffa2bf
HttpMarco <mirco.lindenau@gmx.de>
2024-05-04 14:41:42 +0200

    Add mariadb detector value manipulation

--- evelon-sql-mariadb/src/main/java/dev/httpmarco/evelon/MariaDbLayer.java
+++ evelon-sql-mariadb/src/main/java/dev/httpmarco/evelon/MariaDbLayer.java
@@ -2,10 +2,21 @@ package dev.httpmarco.evelon;
 
 import dev.httpmarco.evelon.sql.parent.HikariDefaultAuthentication;
 import dev.httpmarco.evelon.sql.parent.HikariParentConnectionLayer;
+import dev.httpmarco.evelon.sql.parent.types.Type;
 
 public final class MariaDbLayer extends HikariParentConnectionLayer<HikariDefaultAuthentication> {
 
     public MariaDbLayer() {
         super(new HikariDefaultAuthentication("MARIADB", false));
+
+        detector().overwrite(Type.of("VARCHAR(128)", it -> it.clazz().equals(String.class) && it.hasConstant(RepositoryConstant.PRIMARY_KEY)));
+
+        detector().overwrite(Type.of("BOOL", it -> {
+            if (!(it.clazz().equals(boolean.class) || it.clazz().equals(Boolean.class))) {
+                return false;
+            }
+            it.constant(RepositoryConstant.VALUE_REWRITING, o -> ((boolean) o) ? 1 : 0);
+            return true;
+        }));
     }
 }
