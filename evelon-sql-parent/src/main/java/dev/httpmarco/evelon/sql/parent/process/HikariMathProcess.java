765a713da8ee30f5068d3afb28ba1f1bb74b61a1
HttxMarco <mircolindenau04@gmail.com>
2024-05-07 14:16:05 +0200

    Add a general math process

---
+++ evelon-sql-parent/src/main/java/dev/httpmarco/evelon/sql/parent/process/HikariMathProcess.java
@@ -0,0 +1,28 @@
+package dev.httpmarco.evelon.sql.parent.process;
+
+import dev.httpmarco.evelon.RepositoryExternalEntry;
+import dev.httpmarco.evelon.filtering.Filter;
+import dev.httpmarco.evelon.process.kind.QueryProcess;
+import dev.httpmarco.evelon.sql.parent.HikariFilter;
+import dev.httpmarco.evelon.sql.parent.reference.HikariProcessReference;
+import lombok.AllArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+@AllArgsConstructor
+public final class HikariMathProcess extends QueryProcess<HikariProcessReference, HikariFilter<Object>> {
+
+    private static final String MATH_QUERY = "SELECT %s AS data FROM %s;";
+    private String type;
+
+    @Override
+    public Object run(@NotNull RepositoryExternalEntry entry, @NotNull HikariProcessReference reference) {
+        var data = new AtomicReference<>();
+
+        reference.append(MATH_QUERY.formatted(type, entry.id()), filters().stream().map(Filter::value).toArray(), resultSet -> {
+            data.set(resultSet.getObject("data"));
+        });
+        return data;
+    }
+}
