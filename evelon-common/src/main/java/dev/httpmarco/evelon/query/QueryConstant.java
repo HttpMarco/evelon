82ac66e7ab57e871b800db0d0aefce3d78ec362e
HttxMarco <mircolindenau04@gmail.com>
2024-05-07 12:04:28 +0200

    First implementation of query constants

---
+++ evelon-common/src/main/java/dev/httpmarco/evelon/query/QueryConstant.java
@@ -0,0 +1,11 @@
+package dev.httpmarco.evelon.query;
+
+import dev.httpmarco.evelon.constant.Constant;
+
+public final class QueryConstant<T> extends Constant<T> {
+
+    public QueryConstant(String id) {
+        super(id);
+    }
+
+}
