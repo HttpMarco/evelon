9394d800a877b38d957203ee8991d8d4923ddec1
HttxMarco <mircolindenau04@gmail.com>
2024-05-07 12:53:22 +0200

    Set process properties deprecated

--- evelon-common/src/main/java/dev/httpmarco/evelon/process/Process.java
+++ evelon-common/src/main/java/dev/httpmarco/evelon/process/Process.java
@@ -1,5 +1,6 @@
 package dev.httpmarco.evelon.process;
 
+import dev.httpmarco.evelon.constant.ConstantPool;
 import dev.httpmarco.evelon.filtering.Filter;
 import lombok.Getter;
 import lombok.experimental.Accessors;
@@ -7,23 +8,28 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.*;
 
+@Getter
 @Accessors(fluent = true)
 public abstract class Process<F extends Filter<?, ?>> {
 
-    @Getter
     private final List<F> filters = new ArrayList<>();
 
+    private final ConstantPool constants = new ConstantPool();
+
     // for example for put the foreign value in the next sub process
+    @Deprecated
     private final Map<Object, Object> properties = new LinkedHashMap<>();
 
+
+    @Deprecated
     public void property(@NotNull Object key, Object value) {
         properties.put(key.toString(), value);
     }
 
+    @Deprecated
     public Object property(@NotNull Object key) {
         return properties.get(key.toString());
     }
-
 
     @SuppressWarnings("unchecked")
     public void appendFilters(List<Filter<?, ?>> filters) {
