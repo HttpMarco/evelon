1030ba38ee1523d2e37758804b43d824bd6106e6
HttxMarco <mircolindenau04@gmail.com>
2024-05-07 17:36:27 +0200

    Improve tests

---
+++ evelon-demo/src/test/java/dev/httpmarco/evelon/demo/EvelonTest.java
@@ -0,0 +1,84 @@
+package dev.httpmarco.evelon.demo;
+
+import dev.httpmarco.evelon.Repository;
+import dev.httpmarco.evelon.demo.models.SimpleModel;
+import dev.httpmarco.evelon.demo.models.objects.EnumObject;
+import dev.httpmarco.evelon.sql.h2.H2Layer;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.UUID;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Nested
+@DisplayName("evelon default query methods")
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class EvelonTest {
+
+    @Contract(pure = true)
+    static @NotNull Stream<Arguments> repositoryProvider() {
+        return Stream.of(
+                Arguments.of(Repository.build(SimpleModel.class).withLayer(H2Layer.class).build(),
+                        new SimpleModel(
+                                'c',
+                                9,
+                                10,
+                                UUID.randomUUID(),
+                                false,
+                                "test streatt",
+                                EnumObject.COOKIE)
+                )
+        );
+    }
+
+
+    @Order(0)
+    @ParameterizedTest
+    @MethodSource("repositoryProvider")
+    <T> void create(@NotNull Repository<T> repository, T dummy) {
+        repository.query().create(dummy);
+    }
+
+    @Order(1)
+    @ParameterizedTest
+    @MethodSource("repositoryProvider")
+    <T> void count(@NotNull Repository<T> repository) {
+        assertEquals(1, repository.query().count());
+    }
+
+    @Order(2)
+    @ParameterizedTest
+    @MethodSource("repositoryProvider")
+    <T> void exists(@NotNull Repository<T> repository) {
+        assertTrue(repository.query().exists());
+    }
+
+    @Order(3)
+    @ParameterizedTest
+    @MethodSource("repositoryProvider")
+    void sum(@NotNull Repository<?> repository) {
+        assertNotSame(-1, repository.query(H2Layer.class).sum("age"));
+        assertNotSame(0, repository.query(H2Layer.class).sum("age"));
+    }
+
+    @Order(4)
+    @ParameterizedTest
+    @MethodSource("repositoryProvider")
+    void average(@NotNull Repository<?> repository) {
+        assertNotSame(-1, repository.query(H2Layer.class).average("age"));
+        assertNotSame(0, repository.query(H2Layer.class).average("age"));
+    }
+
+    @Order(18)
+    @ParameterizedTest
+    @MethodSource("repositoryProvider")
+    void delete(@NotNull Repository<?> repository) {
+        repository.query().delete();
+    }
+}
--- evelon-demo/src/test/java/dev/httpmarco/evelon/demo/H2DatabaseTest.java
+++ evelon-demo/src/test/java/dev/httpmarco/evelon/demo/H2DatabaseTest.java
@@ -13,48 +13,16 @@ import dev.httpmarco.evelon.sql.h2.H2Layer;
 import org.junit.jupiter.api.*;
 
 import java.util.List;
-import java.util.Map;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public final class H2DatabaseTest {
-
     @Nested
     @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
     @DisplayName("H2 - Object - Simple repository Test")
     class SimpleModelTest {
-
         private static Repository<SimpleModel> REPOSITORY;
-        private static final SimpleModel DUMMY = new SimpleModel('a', 8, 2000, UUID.randomUUID(), false, "test street", EnumObject.RED);
-        private static final SimpleModel DUMMY2 = new SimpleModel('z', 99, 100, UUID.randomUUID(), true, "a street", EnumObject.COOKIE);
-
-
-        @Test
-        @Order(0)
-        void initialize() {
-            assertNotNull(REPOSITORY = Repository.build(SimpleModel.class).withId("persons").withLayer(H2Layer.class).build());
-        }
-
-        @Test
-        @Order(1)
-        void create() {
-            REPOSITORY.query().create(DUMMY);
-            REPOSITORY.query().create(DUMMY2);
-        }
-
-        @Test
-        @Order(2)
-        void count() {
-            assertEquals(2, REPOSITORY.query(H2Layer.class).count());
-        }
-
-        @Test
-        @Order(3)
-        void exists() {
-            assertTrue(REPOSITORY.query().exists());
-        }
-
         @Test
         @Order(4)
         void find() {
@@ -126,178 +94,6 @@ public final class H2DatabaseTest {
             var values = REPOSITORY.query().order("money", Ordering.DESCENDING);
 
             assertFalse(values.isEmpty());
-        }
-
-
-        @Test
-        @Order(12)
-        void sum() {
-            assertEquals(14, REPOSITORY.query(H2Layer.class).sum("age"));
-        }
-
-        @Test
-        @Order(13)
-        void average() {
-            assertEquals(7.0, REPOSITORY.query(H2Layer.class).average("age"));
-        }
-
-        @Test
-        @Order(14)
-        void min() {
-            //todo
-            assertTrue(false);
-        }
-
-        @Test
-        @Order(15)
-        void max() {
-            //todo
-            assertTrue(false);
-        }
-
-        @Test
-        @Order(18)
-        void delete() {
-            REPOSITORY.query().delete();
-        }
-    }
-
-    @Nested
-    @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-    @DisplayName("H2 - Collection - Simple repository Test")
-    public class SimpleCollectionModelText {
-
-        private static Repository<SimpleCollectionModel> REPOSITORY;
-        private static final SimpleCollectionModel DUMMY = new SimpleCollectionModel(UUID.randomUUID(), List.of("a", "b"), List.of("admin", "test"));
-
-        @Test
-        @Order(0)
-        void initialize() {
-            assertNotNull(REPOSITORY = Repository.build(SimpleCollectionModel.class).withId("persons_simple").withLayer(H2Layer.class).build());
-        }
-
-        @Test
-        @Order(1)
-        void create() {
-            REPOSITORY.query().create(DUMMY);
-        }
-
-        @Test
-        @Order(2)
-        void count() {
-            assertEquals(1, REPOSITORY.query(H2Layer.class).count());
-        }
-
-        @Test
-        @Order(3)
-        void exists() {
-            assertTrue(REPOSITORY.query().exists());
-        }
-
-        @Test
-        @Order(4)
-        void find() {
-            var values = REPOSITORY.query(H2Layer.class).find();
-
-            assertNotNull(values);
-            assertEquals(1, values.size());
-
-            var model = values.get(0);
-            assertNotNull(model);
-            assertEquals(2, model.getGroups().size());
-            assertEquals("test", model.getGroups().get(0));
-        }
-
-        @Test
-        @Order(4)
-        void findFirst() {
-            var value = REPOSITORY.query(H2Layer.class).findFirst();
-
-            assertNotNull(value);
-            assertEquals(2, value.getGroups().size());
-        }
-
-        @Test
-        @Order(20)
-        void delete() {
-            REPOSITORY.query().delete();
-        }
-    }
-
-    @Nested
-    @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-    @DisplayName("H2 - Collections - Complex repository Test")
-    public class ComplexCollectionModelText {
-
-        private static Repository<ComplexCollectionModel> REPOSITORY;
-        private static final ComplexCollectionModel DUMMY = new ComplexCollectionModel(UUID.randomUUID(), List.of(new TestObject1("abc", 2000), new TestObject1("abc", 2000)), List.of(new TestObject2("admin", -1)));
-
-        @Test
-        @Order(0)
-        void initialize() {
-            assertNotNull(REPOSITORY = Repository.build(ComplexCollectionModel.class).withId("persons_complex").withLayer(H2Layer.class).build());
-        }
-
-        @Test
-        @Order(1)
-        void create() {
-            REPOSITORY.query().create(DUMMY);
-        }
-
-        @Test
-        @Order(2)
-        void count() {
-            assertEquals(1, REPOSITORY.query(H2Layer.class).count());
-        }
-
-        @Test
-        @Order(3)
-        void exists() {
-            assertTrue(REPOSITORY.query().exists());
-        }
-
-        @Test
-        @Order(4)
-        void find() {
-            var values = REPOSITORY.query(H2Layer.class).find();
-
-            assertNotNull(values);
-            assertEquals(1, values.size());
-
-            var model = values.get(0);
-            assertNotNull(model);
-            assertEquals(1, model.getGroups().size());
-            assertNotNull(model.getGroups().get(0));
-            assertEquals(-1, model.getGroups().get(0).getExpire());
-        }
-
-        @Test
-        @Order(4)
-        void findFirst() {
-            var value = REPOSITORY.query(H2Layer.class).findFirst();
-
-            assertNotNull(value);
-            assertEquals(1, value.getGroups().size());
-        }
-
-        @Test
-        @Order(20)
-        void delete() {
-            REPOSITORY.query().delete();
-        }
-    }
-
-    @Nested
-    @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-    @DisplayName("H2 - Maps - Simple key/value repository test")
-    public class MapSimpleTest {
-
-        private static Repository<SimpleMapModel> REPOSITORY;
-        private static final SimpleMapModel DUMMY = new SimpleMapModel(UUID.randomUUID(), "httpmarco", Map.of("auto-update", false, "autostart", true));
-
-        @Test
-        public void initialize() {
-            assertNotNull(REPOSITORY = Repository.build(SimpleMapModel.class).withId("maps_simple").withLayer(H2Layer.class).build());
         }
     }
 }
