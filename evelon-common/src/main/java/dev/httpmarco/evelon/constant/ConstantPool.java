ef01e8832e4377a2b886f2c465ce59c48177387f
HttxMarco <mircolindenau04@gmail.com>
2024-05-07 12:50:42 +0200

    Add clone options for constant pool

--- evelon-common/src/main/java/dev/httpmarco/evelon/constant/ConstantPool.java
+++ evelon-common/src/main/java/dev/httpmarco/evelon/constant/ConstantPool.java
@@ -1,30 +1,33 @@
 package dev.httpmarco.evelon.constant;
 
-import dev.httpmarco.evelon.RepositoryConstant;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public final class ConstantPool {
 
-    private final Map<RepositoryConstant<?>, Object> constants = new ConcurrentHashMap<>();
+    private final Map<Constant<?>, Object> constants = new ConcurrentHashMap<>();
 
-    public void option(RepositoryConstant<Void> constant) {
+    public void option(Constant<Void> constant) {
         constants.put(constant, true);
     }
 
-    public <T> T put(RepositoryConstant<T> constant, T value) {
+    public <T> T put(Constant<T> constant, T value) {
         constants.put(constant, value);
         return value;
     }
 
-    public boolean has(RepositoryConstant<?> constant) {
+    public boolean has(Constant<?> constant) {
         return constants.containsKey(constant);
     }
 
     @SuppressWarnings("unchecked")
-    public <T> T constant(RepositoryConstant<T> constant) {
+    public <T> T constant(Constant<T> constant) {
         return (T) constants.get(constant);
     }
 
+    public void cloneConstants(@NotNull ConstantPool pool) {
+        this.constants.putAll(pool.constants);
+    }
 }
