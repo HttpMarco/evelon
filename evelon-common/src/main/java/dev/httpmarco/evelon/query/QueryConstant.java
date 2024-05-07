9f66539289f26e7c8351d56860b37fcf3621fbf5
HttxMarco <mircolindenau04@gmail.com>
2024-05-07 12:51:34 +0200

    Add limit and ordering options

--- evelon-common/src/main/java/dev/httpmarco/evelon/query/QueryConstant.java
+++ evelon-common/src/main/java/dev/httpmarco/evelon/query/QueryConstant.java
@@ -1,6 +1,9 @@
 package dev.httpmarco.evelon.query;
 
+import dev.httpmarco.evelon.Ordering;
 import dev.httpmarco.evelon.constant.Constant;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
 
 public final class QueryConstant<T> extends Constant<T> {
 
@@ -8,4 +11,12 @@ public final class QueryConstant<T> extends Constant<T> {
         super(id);
     }
 
+    public static final QueryConstant<Integer> LIMIT = constant("LIMIT");
+
+    public static final QueryConstant<Ordering> ORDERING = constant("ORDERING");
+
+    @Contract("_ -> new")
+    private static <T> @NotNull QueryConstant<T> constant(String id) {
+        return new QueryConstant<>(id);
+    }
 }
