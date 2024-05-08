ce89336c41e6d1ffd23f60414788b7a3e6e0fabe
HttpMarco <mirco.lindenau@gmx.de>
2024-05-08 19:30:32 +0200

    Update version

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ allprojects {
     apply(plugin = "java-library")
     apply(plugin = "maven-publish")
 
-    version = "1.0.9-SNAPSHOT"
+    version = "1.0.10-SNAPSHOT"
     group = "dev.httpmarco.evelon"
 
     repositories {
