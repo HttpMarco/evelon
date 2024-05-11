2485887d2381fff7322e9fc73d0f894a9ec4be3f
HttxMarco <mircolindenau04@gmail.com>
2024-05-11 19:07:29 +0200

    Fix count and check process (filtering)

--- evelon-sql-parent/src/main/java/dev/httpmarco/evelon/sql/parent/process/HikariCheckProcess.java
+++ evelon-sql-parent/src/main/java/dev/httpmarco/evelon/sql/parent/process/HikariCheckProcess.java
@@ -3,6 +3,7 @@ package dev.httpmarco.evelon.sql.parent.process;
 import dev.httpmarco.evelon.RepositoryExternalEntry;
 import dev.httpmarco.evelon.process.kind.QueryProcess;
 import dev.httpmarco.evelon.sql.parent.HikariFilter;
+import dev.httpmarco.evelon.sql.parent.HikariFilterUtil;
 import dev.httpmarco.evelon.sql.parent.reference.HikariProcessReference;
 import org.jetbrains.annotations.NotNull;
 
@@ -10,12 +11,12 @@ import java.util.concurrent.atomic.AtomicBoolean;
 
 public final class HikariCheckProcess extends QueryProcess<HikariProcessReference, HikariFilter<Object>> {
 
-    private static final String CHECK_QUERY = "SELECT * FROM %s LIMIT 1;";
+    private static final String CHECK_QUERY = "SELECT * FROM %s LIMIT 1";
 
     @Override
     public @NotNull Object run(@NotNull RepositoryExternalEntry entry, @NotNull HikariProcessReference reference) {
         var result = new AtomicBoolean();
-        reference.append(CHECK_QUERY.formatted(entry.id()), it -> result.set(true));
+        reference.append( HikariFilterUtil.appendFiltering(CHECK_QUERY.formatted(entry.id()) + ";", filters()), it -> result.set(true));
         return result;
     }
 }
--- evelon-sql-parent/src/main/java/dev/httpmarco/evelon/sql/parent/process/HikariCountProcess.java
+++ evelon-sql-parent/src/main/java/dev/httpmarco/evelon/sql/parent/process/HikariCountProcess.java
@@ -3,18 +3,19 @@ package dev.httpmarco.evelon.sql.parent.process;
 import dev.httpmarco.evelon.RepositoryExternalEntry;
 import dev.httpmarco.evelon.process.kind.QueryProcess;
 import dev.httpmarco.evelon.sql.parent.HikariFilter;
+import dev.httpmarco.evelon.sql.parent.HikariFilterUtil;
 import dev.httpmarco.evelon.sql.parent.reference.HikariProcessReference;
 import org.jetbrains.annotations.NotNull;
 import java.util.concurrent.atomic.AtomicLong;
 
 public final class HikariCountProcess extends QueryProcess<HikariProcessReference, HikariFilter<Object>> {
 
-    private static final String COUNT_QUERY = "SELECT COUNT(*) AS ELEMENTS FROM %s;";
+    private static final String COUNT_QUERY = "SELECT COUNT(*) AS ELEMENTS FROM %s";
 
     @Override
     public @NotNull Object run(@NotNull RepositoryExternalEntry entry, @NotNull HikariProcessReference reference) {
         AtomicLong count = new AtomicLong(-1);
-        reference.append(String.format(COUNT_QUERY, entry.id()), resultSet -> count.set(resultSet.getLong("ELEMENTS")));
+        reference.append(HikariFilterUtil.appendFiltering(COUNT_QUERY.formatted(entry.id()) + ";", filters()), resultSet -> count.set(resultSet.getLong("ELEMENTS")));
         return count;
     }
 }
