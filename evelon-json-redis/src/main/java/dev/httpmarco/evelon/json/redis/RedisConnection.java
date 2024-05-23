77f3173f0dab36e1f03274765c2de0cc8387eb2e
HttpMarco <mirco.lindenau@gmx.de>
2024-05-24 00:06:59 +0200

    better redis connector

--- evelon-json-redis/src/main/java/dev/httpmarco/evelon/json/redis/RedisConnection.java
+++ evelon-json-redis/src/main/java/dev/httpmarco/evelon/json/redis/RedisConnection.java
@@ -3,32 +3,49 @@ package dev.httpmarco.evelon.json.redis;
 import dev.httpmarco.evelon.layer.connection.Connection;
 import dev.httpmarco.evelon.layer.connection.ConnectionAuthentication;
 import io.lettuce.core.RedisClient;
+import io.lettuce.core.RedisURI;
+import io.lettuce.core.api.StatefulRedisConnection;
 import org.jetbrains.annotations.NotNull;
 
 // todo change object
-public final class RedisConnection implements Connection<RedisClient, Object> {
+public final class RedisConnection implements Connection<StatefulRedisConnection<String, String>, Object> {
 
     private RedisClient client;
+    private StatefulRedisConnection<String, String> connection;
 
     @Override
     public void connect(ConnectionAuthentication credentials) {
-        //todo add crendetials
-        client = RedisClient.create("redis://localhost:6379");
+
+        var auth = ((RedisAuthentication) credentials);
+
+        this.client = RedisClient.create(RedisURI.Builder.redis("localhost")
+                .withSsl(auth.useSsl())
+                .withHost(auth.hostname())
+                .withPort(auth.port())
+                .withPassword(auth.password().toCharArray())
+                .build());
+
+        connection = client.connect();
     }
 
     @Override
     public boolean isConnected() {
-        return client != null && client.connect().isOpen();
+        return connection != null && connection.isOpen();
     }
 
     @Override
     public void close() {
-        client.shutdown();
+        if (connection != null) {
+            this.connection.close();
+        }
+        if(client != null) {
+            this.client.shutdown();
+        }
     }
 
     @Override
-    public RedisClient connection() {
-        return client;
+    public StatefulRedisConnection<String, String> connection() {
+        return this.connection;
     }
 
     @Override
