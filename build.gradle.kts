91e8ef69235cc8417204c2653516c483b9153103
HttpMarco <mirco.lindenau@gmx.de>
2024-05-25 13:43:01 +0200

    Set version to 1.0.28-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ allprojects {
     apply(plugin = "java-library")
     apply(plugin = "maven-publish")
 
-    version = "1.0.27-SNAPSHOT"
+    version = "1.0.28-SNAPSHOT"
     group = "dev.httpmarco.evelon"
 
     repositories {
