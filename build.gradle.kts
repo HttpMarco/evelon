65261850a1495209e8a15e5f0fb7a457f5f7be7e
HttpMarco <mirco.lindenau@gmx.de>
2024-05-27 13:57:31 +0200

    Set version to 1.0.34

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ allprojects {
     apply(plugin = "java-library")
     apply(plugin = "maven-publish")
 
-    version = "1.0.33-SNAPSHOT"
+    version = "1.0.34-SNAPSHOT"
     group = "dev.httpmarco.evelon"
 
     repositories {
