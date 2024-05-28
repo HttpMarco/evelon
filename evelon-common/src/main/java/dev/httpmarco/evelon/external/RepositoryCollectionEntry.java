9abe572230d4b0a9e796b732577b7254a4ac5dff
HttxMarco <mircolindenau04@gmail.com>
2024-05-28 11:15:49 +0200

    Improve repository collection entry

--- evelon-common/src/main/java/dev/httpmarco/evelon/external/RepositoryCollectionEntry.java
+++ evelon-common/src/main/java/dev/httpmarco/evelon/external/RepositoryCollectionEntry.java
@@ -18,16 +18,21 @@ public final class RepositoryCollectionEntry extends RepositoryExternalEntry {
     private final RepositoryEntry typeEntry;
 
     public RepositoryCollectionEntry(String id, @NotNull Field field, RepositoryExternalEntry parent) {
+        this(id, field, parent, Reflections.on(field).generic(0));
+    }
+
+    public RepositoryCollectionEntry(String id, @NotNull Field field, RepositoryExternalEntry parent, Class<?> typeEntryClass) {
         super(id, field.getType(), parent);
 
-        this.typeEntry = RepositoryEntryFinder.find(Reflections.on(field).generic(0), null, field.getName(), this);
+        this.typeEntry = RepositoryEntryFinder.find(typeEntryClass, null, field.getName(), this);
 
-        if (!(typeEntry instanceof RepositoryExternalEntry)) {
+        if (!(this.typeEntry instanceof RepositoryExternalEntry)) {
             this.children(typeEntry);
         } else {
             this.children().addAll(((RepositoryExternalEntry) typeEntry).children());
         }
     }
+
 
     @Override
     @SuppressWarnings("unchecked")
