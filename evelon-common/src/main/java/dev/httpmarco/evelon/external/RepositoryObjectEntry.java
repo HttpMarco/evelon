639367c962764e1304b49d2c62883e29aba67280
HttpMarco <mirco.lindenau@gmx.de>
2024-06-02 20:37:00 +0200

    Add entity: Ignore optional superclass

--- evelon-common/src/main/java/dev/httpmarco/evelon/external/RepositoryObjectEntry.java
+++ evelon-common/src/main/java/dev/httpmarco/evelon/external/RepositoryObjectEntry.java
@@ -3,7 +3,9 @@ package dev.httpmarco.evelon.external;
 import dev.httpmarco.evelon.*;
 import dev.httpmarco.osgan.reflections.Reflections;
 import lombok.Getter;
+import org.jetbrains.annotations.NotNull;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 
 @Getter
@@ -12,34 +14,14 @@ public final class RepositoryObjectEntry extends RepositoryExternalEntry {
     public RepositoryObjectEntry(String id, Class<?> clazz, RepositoryExternalEntry parent) {
         super(id, clazz, parent);
 
-        for (var field : Reflections.on(clazz).allFields()) {
-
-            // evelon does not allow static fields, only object parameters
-            if(Modifier.isStatic(field.getModifiers())) {
-                continue;
+        if (!clazz.isAnnotationPresent(Entity.class) || !clazz.getDeclaredAnnotation(Entity.class).ignoreSuperClass()) {
+            for (var field : Reflections.on(clazz).allFields()) {
+                applyField(field);
             }
-
-            var fieldId = field.getName();
-
-            if (field.isAnnotationPresent(Row.class)) {
-                var row = field.getDeclaredAnnotation(Row.class);
-
-                if (row.ignore()) {
-                    continue;
-                }
-
-                if (!row.id().isEmpty()) {
-                    fieldId = row.id();
-                }
+        } else {
+            for (var field : Reflections.on(clazz).fields()) {
+                applyField(field);
             }
-
-            var entry = RepositoryEntryFinder.find(field, fieldId, this);
-            entry.constants().put(RepositoryConstant.PARAM_FIELD, field);
-
-            if (field.isAnnotationPresent(PrimaryKey.class)) {
-                entry.constants().option(RepositoryConstant.PRIMARY_KEY);
-            }
-            children(entry);
         }
 
         for (var child : children()) {
@@ -48,5 +30,34 @@ public final class RepositoryObjectEntry extends RepositoryExternalEntry {
             }
             child.constants().put(RepositoryConstant.FOREIGN_REFERENCE, primaries());
         }
+    }
+
+    private void applyField(@NotNull Field field) {
+        // evelon does not allow static fields, only object parameters
+        if (Modifier.isStatic(field.getModifiers())) {
+            return;
+        }
+
+        var fieldId = field.getName();
+
+        if (field.isAnnotationPresent(Row.class)) {
+            var row = field.getDeclaredAnnotation(Row.class);
+
+            if (row.ignore()) {
+                return;
+            }
+
+            if (!row.id().isEmpty()) {
+                fieldId = row.id();
+            }
+        }
+
+        var entry = RepositoryEntryFinder.find(field, fieldId, this);
+        entry.constants().put(RepositoryConstant.PARAM_FIELD, field);
+
+        if (field.isAnnotationPresent(PrimaryKey.class)) {
+            entry.constants().option(RepositoryConstant.PRIMARY_KEY);
+        }
+        children(entry);
     }
 }
