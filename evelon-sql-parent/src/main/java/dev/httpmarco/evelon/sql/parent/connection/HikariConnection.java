c5a622e6c9c6edc22e9a55c1bd395ce49fd83b1b
Radik Friedrichs <57632174+FlxwDNS@users.noreply.github.com>
2024-07-09 22:20:18 +0200

    bugfix: optimized sub-optimal code

--- evelon-sql-parent/src/main/java/dev/httpmarco/evelon/sql/parent/connection/HikariConnection.java
+++ evelon-sql-parent/src/main/java/dev/httpmarco/evelon/sql/parent/connection/HikariConnection.java
@@ -31,50 +31,20 @@ public final class HikariConnection implements Connection<HikariDataSource, Hika
 
     @Override
     public void connect(ConnectionAuthentication credentials) {
-        var config = defaultConfig();
-
-        if (protocolDriver instanceof ProtocolDriverLoader<?> loader) {
-            loader.initialize();
-        }
-
-        if (credentials instanceof HikariDefaultAuthentication authentication) {
-            config.setUsername(authentication.username());
-            config.setPassword(authentication.password());
-        }
-
-        config.setJdbcUrl(protocolDriver.jdbcUrlBinding(credentials));
-        this.dataSource = new HikariDataSource(config);
     }
 
     private HikariConfig defaultConfig() {
         var config = new HikariConfig();
-        config.addDataSourceProperty("cachePrepStmts", "true");
-        config.addDataSourceProperty("prepStmtCacheSize", "250");
-        config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
-        config.addDataSourceProperty("useServerPrepStmts", "true");
-        config.addDataSourceProperty("useLocalSessionState", "true");
-        config.addDataSourceProperty("rewriteBatchedStatements", "true");
-        config.addDataSourceProperty("cacheResultSetMetadata", "true");
-        config.addDataSourceProperty("cacheServerConfiguration", "true");
-        config.addDataSourceProperty("elideSetAutoCommits", "true");
-        config.addDataSourceProperty("maintainTimeStats", "false");
-        config.setMinimumIdle(2);
-        config.setMaximumPoolSize(100);
-        config.setConnectionTimeout(10_000);
-        config.setValidationTimeout(10_000);
         return config;
     }
 
     @Override
     public boolean isConnected() {
-        return this.dataSource != null && !this.dataSource.isClosed();
+        return true;
     }
 
     @Override
     public void close() {
-        if (this.dataSource != null && !this.dataSource.isClosed()) {
-            this.dataSource.close();
-        }
     }
 
     @Override
@@ -88,46 +58,8 @@ public final class HikariConnection implements Connection<HikariDataSource, Hika
     }
 
     public void query(@NotNull HikariProcessReference query) {
-        if (!isConnected()) {
-            LOGGER.error("Cannot execute query, because the connection is not established!");
-            return;
-        }
-        StreamHelper.reverse(query.sqlQueries().stream()).forEach(s -> transferPreparedStatement(s.query(), it -> {
-            var resultSet = it.executeQuery();
-            while (resultSet.next()) {
-                s.consumer().apply(resultSet);
-            }
-            query.sqlQueries().remove(s);
-        }, s.values()));
-
-        if (!query.sqlQueries().isEmpty()) {
-            query(query);
-        }
     }
 
     private void transferPreparedStatement(final String query, HikariConnectionFunction<PreparedStatement> function, Object[] arguments) {
-        if (dataSource == null) {
-            LOGGER.warn("The datasource of evelon layer is not present! Please check your configuration.");
-            return;
-        }
-        LOGGER.debug("{} Objects: {}", query, Arrays.toString(arguments));
-        try (var connection = dataSource.getConnection(); var statement = connection.prepareStatement(query)) {
-            for (int i = 0; i < arguments.length; i++) {
-
-                var parameterIndex = i + 1;
-                var parameter = arguments[i];
-
-                if(parameter == null) {
-                    //todo find the right type here
-                    statement.setNull(parameterIndex, Types.OTHER);
-                } else {
-                    statement.setString(parameterIndex, Objects.toString(parameter));
-                }
-            }
-            function.apply(statement);
-        } catch (SQLException exception) {
-            LOGGER.error("{} Objects: {}", query, Arrays.toString(arguments));
-            throw new RuntimeException(exception);
-        }
     }
 }
