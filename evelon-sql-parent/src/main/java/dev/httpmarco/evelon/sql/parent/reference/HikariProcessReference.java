1c6946012e629d3ce919a35c9380919d3b8395a7
HttxMarco <mircolindenau04@gmail.com>
2024-07-11 10:14:39 +0200

    @Deprecate some old methods

--- evelon-sql-parent/src/main/java/dev/httpmarco/evelon/sql/parent/reference/HikariProcessReference.java
+++ evelon-sql-parent/src/main/java/dev/httpmarco/evelon/sql/parent/reference/HikariProcessReference.java
@@ -5,10 +5,9 @@ import dev.httpmarco.evelon.sql.parent.connection.HikariConnection;
 import dev.httpmarco.evelon.sql.parent.connection.HikariConnectionFunction;
 import lombok.Getter;
 import lombok.experimental.Accessors;
-import org.jetbrains.annotations.NotNull;
-
 import java.sql.ResultSet;
 import java.util.*;
+import java.util.function.Function;
 
 @Getter
 @Accessors(fluent = true)
@@ -20,19 +19,28 @@ public final class HikariProcessReference extends ProcessReference<HikariConnect
         super(connection);
     }
 
+    public <R> R directly(String query, Object[] arguments, Function<ResultSet, R> consumer) {
+        return this.connection().query(query, arguments, consumer);
+    }
+
+    @Deprecated
     public HikariProcessReference append(String query, Object[] parameters, HikariConnectionFunction<ResultSet> consumer) {
         this.sqlQueries.add(new HikariReferenceData<>(query, parameters, consumer));
         return this;
     }
 
+    @Deprecated
     public HikariProcessReference append(String query, HikariConnectionFunction<ResultSet> consumer) {
         return this.append(query, new Object[0], consumer);
     }
 
+    @Deprecated
     public HikariProcessReference append(String query, Object[] parameters) {
-        return this.append(query, parameters, resultSet -> {});
+        return this.append(query, parameters, resultSet -> {
+        });
     }
 
+    @Deprecated
     public HikariProcessReference append(String query) {
         return this.append(query, new Object[0]);
     }
