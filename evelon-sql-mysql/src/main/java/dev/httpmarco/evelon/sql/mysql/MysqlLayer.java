25dfeaa300ed533a2f9547a0f2c8c6383ec1bf0b
HttpMarco <mirco.lindenau@gmx.de>
2024-07-31 10:44:26 +0200

    Add transforming for mysql

--- evelon-sql-mysql/src/main/java/dev/httpmarco/evelon/sql/mysql/MysqlLayer.java
+++ evelon-sql-mysql/src/main/java/dev/httpmarco/evelon/sql/mysql/MysqlLayer.java
@@ -1,8 +1,12 @@
 package dev.httpmarco.evelon.sql.mysql;
 
+import dev.httpmarco.evelon.RepositoryConstant;
 import dev.httpmarco.evelon.sql.parent.HikariDefaultAuthentication;
 import dev.httpmarco.evelon.sql.parent.HikariParentConnectionLayer;
+import dev.httpmarco.evelon.sql.parent.types.Type;
 import lombok.SneakyThrows;
+
+import java.util.UUID;
 
 public final class MysqlLayer extends HikariParentConnectionLayer<HikariDefaultAuthentication> {
 
@@ -12,6 +16,21 @@ public final class MysqlLayer extends HikariParentConnectionLayer<HikariDefaultA
 
         Class.forName("com.mysql.cj.jdbc.Driver");
 
-       // detector().overwrite(Type.of("VARCHAR(128)", it -> it.clazz().equals(String.class) && it.constants().has(RepositoryConstant.PRIMARY_KEY)));
+        detector().overwrite(Type.of("CHAR(36)", it -> {
+
+            if (it.clazz().equals(UUID.class)) {
+                // it.constants().put(RepositoryConstant.VALUE_REFACTOR, o -> ((boolean) o) ? 1 : 0);
+                return true;
+            }
+            return false;
+        }));
+
+        detector().overwrite(Type.of("BOOL", it -> {
+            if (!(it.clazz().equals(boolean.class) || it.clazz().equals(Boolean.class))) {
+                return false;
+            }
+            it.constants().put(RepositoryConstant.VALUE_REFACTOR, o -> ((boolean) o) ? 1 : 0);
+            return true;
+        }));
     }
 }
