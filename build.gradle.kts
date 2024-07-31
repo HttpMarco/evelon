dc161c782eb6f07b39ff69f1c93102fc8f60d1f6
HttpMarco <mirco.lindenau@gmx.de>
2024-07-31 13:30:39 +0200

    Change version to 1.0.46

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ allprojects {
     apply(plugin = "java-library")
     apply(plugin = "maven-publish")
 
-    version = "1.0.45-SNAPSHOT"
+    version = "1.0.46-SNAPSHOT"
     group = "dev.httpmarco.evelon"
 
     repositories {
