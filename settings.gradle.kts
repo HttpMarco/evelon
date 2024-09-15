b122ed7196b7b9cd322619814466ed975913ebc2
HttpMarco <mirco.lindenau@gmx.de>
2024-09-15 22:00:06 +0200

    Implement abstract document module

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,9 +2,12 @@ rootProject.name = "evelon"
 
 include("evelon-common")
 include("evelon-demo")
+
 include("evelon-sql-parent")
 include("evelon-sql-h2")
 include("evelon-sql-mariadb")
 include("evelon-sql-mysql")
 include("evelon-sql-timescaledb")
 include("evelon-sql-postgresql")
+
+include("evelon-document-parent")
