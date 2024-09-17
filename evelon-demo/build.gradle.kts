8b267d6e02aa92e80855b2b2f5f28e73bca36fa6
HttpMarco <mirco.lindenau@gmx.de>
2024-09-17 17:13:46 +0200

    Add redis demo

--- evelon-demo/build.gradle.kts
+++ evelon-demo/build.gradle.kts
@@ -10,6 +10,8 @@ dependencies {
     testImplementation(project(":evelon-sql-mysql"))
     testImplementation(project(":evelon-sql-postgresql"))
 
+    testImplementation(project(":evelon-document-redis"))
+
     testImplementation(platform("org.junit:junit-bom:5.10.3"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
