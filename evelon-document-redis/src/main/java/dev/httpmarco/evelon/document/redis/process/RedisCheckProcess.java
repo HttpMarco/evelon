77741443cc5831a9c3d61ed7f2dafe112044d817
HttpMarco <mirco.lindenau@gmx.de>
2024-09-17 23:32:27 +0200

    Change redis class changeability

--- evelon-document-redis/src/main/java/dev/httpmarco/evelon/document/redis/process/RedisCheckProcess.java
+++ evelon-document-redis/src/main/java/dev/httpmarco/evelon/document/redis/process/RedisCheckProcess.java
@@ -5,7 +5,7 @@ import dev.httpmarco.evelon.document.redis.RedisFilter;
 import dev.httpmarco.evelon.document.redis.RedisProcessReference;
 import dev.httpmarco.evelon.process.kind.QueryProcess;
 
-public class RedisCheckProcess extends QueryProcess<Boolean, RedisProcessReference, RedisFilter<Object>> {
+public final class RedisCheckProcess extends QueryProcess<Boolean, RedisProcessReference, RedisFilter<Object>> {
 
     @Override
     public Boolean run(RepositoryExternalEntry entry, RedisProcessReference reference) {
--- evelon-document-redis/src/main/java/dev/httpmarco/evelon/document/redis/process/RedisSearchProcess.java
+++ evelon-document-redis/src/main/java/dev/httpmarco/evelon/document/redis/process/RedisSearchProcess.java
@@ -5,7 +5,8 @@ import dev.httpmarco.evelon.document.redis.RedisFilter;
 import dev.httpmarco.evelon.document.redis.RedisProcessReference;
 import dev.httpmarco.evelon.process.kind.QueryProcess;
 
-public class RedisSearchProcess extends QueryProcess<Boolean, RedisProcessReference, RedisFilter<Object>> {
+public final class RedisSearchProcess extends QueryProcess<Boolean, RedisProcessReference, RedisFilter<Object>> {
+
     @Override
     public Boolean run(RepositoryExternalEntry entry, RedisProcessReference reference) {
         var content = reference.connection().connection().sync().get(entry.id());
