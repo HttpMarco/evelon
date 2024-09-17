8afe95faa9c0affaf52db455fff3384f67019172
HttpMarco <mirco.lindenau@gmx.de>
2024-09-17 21:44:57 +0200

    Attach default json object handling

--- evelon-document-redis/src/main/java/dev/httpmarco/evelon/document/redis/RedisProcessReference.java
+++ evelon-document-redis/src/main/java/dev/httpmarco/evelon/document/redis/RedisProcessReference.java
@@ -1,9 +1,16 @@
 package dev.httpmarco.evelon.document.redis;
 
+import com.google.gson.JsonObject;
 import dev.httpmarco.evelon.document.redis.connection.RedisConnection;
 import dev.httpmarco.evelon.process.ProcessReference;
+import lombok.Getter;
+import lombok.experimental.Accessors;
 
+@Getter
+@Accessors(fluent = true)
 public final class RedisProcessReference extends ProcessReference<RedisConnection> {
+
+    private final JsonObject data = new JsonObject();
 
     public RedisProcessReference(RedisConnection connection) {
         super(connection);
