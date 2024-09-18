bd38a578310144f64a63f134137c337de15169d8
HttpMarco <mirco.lindenau@gmx.de>
2024-09-18 19:43:54 +0200

    Remove json things (redis module)

--- evelon-document-redis/src/main/java/dev/httpmarco/evelon/document/redis/RedisProcessReference.java
+++ evelon-document-redis/src/main/java/dev/httpmarco/evelon/document/redis/RedisProcessReference.java
@@ -1,6 +1,5 @@
 package dev.httpmarco.evelon.document.redis;
 
-import com.google.gson.JsonObject;
 import dev.httpmarco.evelon.document.redis.connection.RedisConnection;
 import dev.httpmarco.evelon.process.ProcessReference;
 import lombok.Getter;
@@ -9,8 +8,6 @@ import lombok.experimental.Accessors;
 @Getter
 @Accessors(fluent = true)
 public final class RedisProcessReference extends ProcessReference<RedisConnection> {
-
-    private final JsonObject data = new JsonObject();
 
     public RedisProcessReference(RedisConnection connection) {
         super(connection);
