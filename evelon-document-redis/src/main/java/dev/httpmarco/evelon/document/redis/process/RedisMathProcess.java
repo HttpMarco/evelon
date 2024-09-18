6f857045858d666e0b88e8ee5e7c5fa3da26c592
HttpMarco <mirco.lindenau@gmx.de>
2024-09-18 19:45:25 +0200

    Start working on redis math process

--- evelon-document-redis/src/main/java/dev/httpmarco/evelon/document/redis/process/RedisMathProcess.java
+++ evelon-document-redis/src/main/java/dev/httpmarco/evelon/document/redis/process/RedisMathProcess.java
@@ -1,4 +1,19 @@
 package dev.httpmarco.evelon.document.redis.process;
 
-public final class RedisMathProcess {
+import dev.httpmarco.evelon.RepositoryExternalEntry;
+import dev.httpmarco.evelon.document.redis.RedisFilter;
+import dev.httpmarco.evelon.document.redis.RedisProcessReference;
+import dev.httpmarco.evelon.process.kind.QueryProcess;
+
+import java.util.List;
+
+public final class RedisMathProcess extends QueryProcess<Object, RedisProcessReference, RedisFilter<Object>>  {
+    @Override
+    public Object run(RepositoryExternalEntry entry, RedisProcessReference reference) {
+
+
+
+
+        return List.of();
+    }
 }
